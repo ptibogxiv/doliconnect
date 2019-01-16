33df1080e9b997f04f57cbd7dce7173c6c27d0dd
ptibogxiv <support@ptibogxiv.net>
2019-01-16 12:35:43 +0100

    fix hybridauth

--- doliconnect.php
+++ doliconnect.php
@@ -727,8 +727,8 @@ echo "</div>";
 }
 }
 } elseif ( isset($_GET["provider"]) ) { 
-include( plugin_dir_path( __DIR__ ) . 'doliconnect-pro/hybridauth/src/autoload.php');
-include( plugin_dir_path( __DIR__ ) . 'doliconnect-pro/hybridauth/src/config.php');
+include( plugin_dir_path( __DIR__ ) . 'doliconnect-pro/includes/hybridauth/src/autoload.php');
+include( plugin_dir_path( __DIR__ ) . 'doliconnect-pro/includes/hybridauth/src/config.php');
 try{
     //Feed configuration array to Hybridauth
     $hybridauth = new Hybridauth\Hybridauth($config);
