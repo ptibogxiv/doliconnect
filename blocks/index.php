048cc63bdc19df9689157448f3b94d797101b50d
ptibogxiv <support@ptibogxiv.net>
2019-01-19 11:49:37 +0100

    fix if < wordpress 5

--- blocks/index.php
+++ blocks/index.php
@@ -1,7 +1,7 @@
 <?php
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
-//if (version_compare(get_bloginfo('version'), '5.0', '>=' ) or is_plugin_active( 'gutenberg/gutenberg.php' )) {
+if ( version_compare(get_bloginfo('version'), '5.0', '>=' ) ) {
  
 function load_doliconnect_bootstrap_admin_style($hook) {
     if ( 'post.php' != $hook ) {
@@ -16,4 +16,5 @@ add_action( 'admin_enqueue_scripts', 'load_doliconnect_bootstrap_admin_style' );
 require_once ( WP_PLUGIN_DIR . '/' . dirname( plugin_basename( __DIR__ ) ) . '/blocks/admin/admin.php' );
 require_once ( WP_PLUGIN_DIR . '/' . dirname( plugin_basename( __DIR__ ) ) . '/blocks/membership/membership.php' );
 require_once ( WP_PLUGIN_DIR . '/' . dirname( plugin_basename( __DIR__ ) ) . '/blocks/product/product.php' );
-//}
+
+}
