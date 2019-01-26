62efa70f4cbed4d63d5b3d79fcfeed6bc0c6ee62
ptibogxiv <support@ptibogxiv.net>
2019-01-26 16:30:41 +0100

    add donate link

--- doliconnect.php
+++ doliconnect.php
@@ -11,7 +11,8 @@
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
  * Text Domain: doliconnect
  * Domain Path: /languages
- *
+ * Donate link: https://www.paypal.me/ptibogxiv
+ *   
  * @author ptibogxiv.net <support@ptibogxiv.net>
  * @copyright Copyright (c) 2017-2019, ptibogxiv.net
 **/
