6dc184968ec96c2d227cca417a2c74cefedcc9ff
ptibogxiv <support@ptibogxiv.net>
2019-02-23 14:07:57 +0100

    Update tools.php

--- functions/tools.php
+++ functions/tools.php
@@ -210,7 +210,7 @@ unlink($file);
 
 }
 
-if ( isset($ref) && isset($fichier) ) { $document = "<a class='btn btn-outline-secondary btn-sm btn-block' href='".esc_url( add_query_arg( array('download' => $ref."/".$fichier, 'securekey' => hash('sha256', $ID.$type.$ref."/".$fichier)), $url) )."' >$name <i class='fas fa-file-pdf' aria-hidden='true'></i></a>"; }
+if ( isset($ref) && isset($fichier) ) { $document = "<a class='btn btn-dark btn-sm btn-block' href='".esc_url( add_query_arg( array('download' => $ref."/".$fichier, 'securekey' => hash('sha256', $ID.$type.$ref."/".$fichier)), $url) )."' >$name <i class='fas fa-file-download'></i></i></a>"; }
 
 return $document;
 }
