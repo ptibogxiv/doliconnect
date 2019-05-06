b1706882fc165299b47249ec5b2e830746c3ecad
ptibogxiv <support@ptibogxiv.net>
2019-05-06 18:56:54 +0200

    Update dashboard.php

--- functions/dashboard.php
+++ functions/dashboard.php
@@ -897,14 +897,26 @@ $doc = array_reverse(explode("/", $invoice->last_main_doc));
 $document_invoice = dolidocdownload($doc[2], $doc[1], $doc[0], $url."&id=".$_GET['id']."&ref=".$orderfo->ref, __( 'Invoice', 'doliconnect' ));
 } else {
 $document_invoice = dolidocdownload('invoice', $invoice->ref, $invoice->ref.'.pdf', $url."&id=".$_GET['id']."&ref=".$orderfo->ref, __( 'Invoice', 'doliconnect' ), true);
-} 
+}
+
+if ( function_exists('dolipaymentmodes') ) {
+
+$payment_invoice = "<a href='".doliconnecturl('dolicart')."?pay&module=invoices&id=".esc_attr($_GET['id'])."&ref=".esc_attr($_GET['ref'])."' id='button-source-payment' class='btn btn-warning btn-block' role='button'><span class='fa fa-credit-card'></span> ".__( 'Pay', 'doliconnect' )."</a><br>";
+
+} elseif ( isset($orderfo->public_payment_url) && !empty($orderfo->public_payment_url) ) {
+
+$payment_invoice = "<a href='".$orderfo->public_payment_url."' id='button-source-payment' class='btn btn-warning btn-block' role='button'><span class='fa fa-credit-card'></span> ".__( 'Pay', 'doliconnect' )."</a><br>";
+
+} else {
+$payment_invoice = null;
+}
   
 $fruits[$invoice->date_creation.'i'] = array(
 "timestamp" => $invoice->date_creation,
 "type" => __( 'Invoice', 'doliconnect' ),  
 "label" => $invoice->ref,
 "document" => $document_invoice,
-"description" => null,
+"description" => $payment_invoice,
 );  
 } 
 } 
