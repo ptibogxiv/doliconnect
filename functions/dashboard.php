b26fefd08ffa5bcdfd4a8be50c0b2277467c4bfc
ptibogxiv <support@ptibogxiv.net>
2019-06-06 15:06:11 +0200

    Update dashboard.php

--- functions/dashboard.php
+++ functions/dashboard.php
@@ -813,7 +813,9 @@ print "</div></div>";
 if (!empty($orderfo->contacts_ids) && is_array($orderfo->contacts_ids)) {
 print "<div class='card-group'>";
 foreach ($orderfo->contacts_ids as $contact) {
-print "<div class='card'><div class='card-body'><h6>".$contact->libelle."</h6>".$contact->civility." ".$contact->firstname." ".$contact->lastname;//<br>".$contact->address."<br>".$contact->zip."".$contact->town.
+print "<div class='card'><div class='card-body'><h6>".$contact->libelle."</h6>";
+print doliaddress($contact);
+//.$contact->civility." ".$contact->firstname." ".$contact->lastname;//<br>".$contact->address."<br>".$contact->zip."".$contact->town.
 print "</div></div>";
 }
 print "</div><br>";
