82589f56af8c0b52834890b6485d94f8356e28c3
ptibogxiv <support@ptibogxiv.net>
2019-06-18 15:06:15 +0200

    Update tools.php

--- functions/tools.php
+++ functions/tools.php
@@ -50,7 +50,9 @@ print "<option value ='".$postadh->id."' ";
 if ( isset($object->typeid) && $object->typeid == $postadh->id && $object->typeid != null ) {
 print "selected ";
 } elseif ( $postadh->family == '1' ) { print "disabled "; }
-print ">".$postadh->label.", ".$postadh->note."</option>";
+print ">".$postadh->label;
+if ( isset($postadh->note) ) { print ", ".$postadh->note; }
+print "</option>";
 }}
 print "</select></div></div></li><li class='list-group-item'>";
 }
