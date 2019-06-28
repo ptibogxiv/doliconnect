9b4ef54682b8e0e3bf6baf85c8145a3402b86125
ptibogxiv <support@ptibogxiv.net>
2019-06-28 17:27:29 +0200

    Update enqueues.php

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -15,7 +15,7 @@ $version='4.3.1';
   
 	wp_enqueue_script( 'bootstrap-js', plugins_url( 'doliconnect/includes/js/scripts.min.js'), array('jquery'), ' ', true );
 
-	wp_enqueue_script( 'font-awesome', '//use.fontawesome.com/releases/v5.9.0/js/all.js', array(), '5.9.0' );
+	wp_enqueue_script( 'fontawesome', '//use.fontawesome.com/releases/v5.9.0/js/all.js', array(), '5.9.0' );
   
   wp_enqueue_style( 'bootstrap-social', plugins_url( 'doliconnect/includes/css/bootstrap-social.css'), array(), $version); 
 
