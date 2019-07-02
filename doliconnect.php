1c996edc1dfc5577448faa91c2f9e52e26e4f595
ptibogxiv <support@ptibogxiv.net>
2019-07-02 20:08:31 +0200

    Update doliconnect.php

--- doliconnect.php
+++ doliconnect.php
@@ -1153,7 +1153,9 @@ if ( defined("DOLIBUG") ) {
 $content .= dolibug();
 
 } elseif (empty($donation->value)) {
+$content .= "<div class='card shadow-sm'><div class='card-body'>";
 $content .= dolibug(__( 'Inactive module on Dolibarr', 'doliconnect' ));
+$content .= "</div></div>";
 } elseif (is_user_logged_in())  {
 
 if ( doliconnector($current_user, 'fk_soc') > '0') {
