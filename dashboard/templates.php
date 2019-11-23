f6db4f9ac6db6668c5a8be37c6f4ae14a6285ba7
ptibogxiv <support@ptibogxiv.net>
2019-11-23 17:29:52 +0100

    Update templates.php

--- dashboard/templates.php
+++ dashboard/templates.php
@@ -1358,7 +1358,7 @@ print "</small></li>";
 print "</ul></div></div><div class='col-12 col-md-8'>";
 
 if ( doliversion('10.0.0') ) {
-print dolipaymentmethods($object, substr($module, 0, -1), doliconnecturl('dolicart')."?pay", esc_attr(isset($_GET["refresh"]) ? $_GET["refresh"] : null));
+print dolipaymentmethods($object, substr($module, 0, -1), doliconnecturl('dolicart')."?pay", true);
 } else {
 print __( "It seems that your version of Dolibarr and/or its plugins are not up to date!", "doliconnect");
 }
