18d233957ee491fd36707b4accdf46024bc923f2
ptibogxiv <support@ptibogxiv.net>
2019-11-24 20:44:54 +0100

    Update tools.php

--- functions/tools.php
+++ functions/tools.php
@@ -1344,21 +1344,33 @@ global $current_user;
 		<?php else : ?>
 <li class='list-group-item list-group-item-action flex-column align-items-start'><div class='custom-control custom-radio'>
 <input id='gdrf-data-type-export' class='custom-control-input' type='radio' name='gdrf_data_type' value='export_personal_data' checked>
-<label class='custom-control-label w-100' for='gdrf-data-type-export'><div class='row'><div class='d-none d-sm-block col-sm-3 col-md-2 align-middle'>
+<label class='custom-control-label w-100' for='gdrf-data-type-export'><div class='row'>
+		<?php if ( !isset($args['widget']) ) : ?>
+<div class='d-none d-sm-block col-sm-3 col-md-2 align-middle'>
 <center><i class='fas fa-download fa-3x fa-fw'></i></center>
-</div><div class='col-auto align-middle'><h6 class='my-0'><?php echo __( 'Export your data', 'doliconnect' ); ?></h6><small class='text-muted'><?php echo __( 'You will receive an email with a secure link to your data', 'doliconnect' ); ?></small>
+</div>
+		<?php endif; ?>
+<div class='col-auto align-middle'><h6 class='my-0'><?php echo __( 'Export your data', 'doliconnect' ); ?></h6><small class='text-muted'><?php echo __( 'You will receive an email with a secure link to your data', 'doliconnect' ); ?></small>
 </div></div></label></div></li>
 <li class='list-group-item list-group-item-action flex-column align-items-start'><div class='custom-control custom-radio'>
 <input id='gdrf-data-type-remove' class='custom-control-input' type='radio' name='gdrf_data_type' value='remove_personal_data' disabled>
-<label class='custom-control-label w-100' for='gdrf-data-type-remove'><div class='row'><div class='d-none d-sm-block col-sm-3 col-md-2 align-middle'>
+<label class='custom-control-label w-100' for='gdrf-data-type-remove'><div class='row'>
+		<?php if ( !isset($args['widget']) ) : ?>
+<div class='d-none d-sm-block col-sm-3 col-md-2 align-middle'>
 <center><i class='fas fa-eraser fa-3x fa-fw'></i></center>
-</div><div class='col-auto align-middle'><h6 class='my-0'><?php echo __( 'Erase your data', 'doliconnect' ); ?></h6><small class='text-muted'><?php echo __( 'Soon, you will be able to erase your account', 'doliconnect' ); ?></small>
+</div>
+		<?php endif; ?>
+<div class='col-auto align-middle'><h6 class='my-0'><?php echo __( 'Erase your data', 'doliconnect' ); ?></h6><small class='text-muted'><?php echo __( 'Soon, you will be able to erase your account', 'doliconnect' ); ?></small>
 </div></div></label></div></li>
 <li class='list-group-item list-group-item-action flex-column align-items-start disabled'><div class='custom-control custom-radio'>
 <input id='gdrf-data-type-delete' class='custom-control-input' type='radio' name='gdrf_data_type' value='delete_personal_data' disabled>
-<label class='custom-control-label w-100' for='gdrf-data-type-delete'><div class='row'><div class='d-none d-sm-block col-sm-3 col-md-2 align-middle'>
+<label class='custom-control-label w-100' for='gdrf-data-type-delete'><div class='row'>
+		<?php if ( !isset($args['widget']) ) : ?>
+<div class='d-none d-sm-block col-sm-3 col-md-2 align-middle'>
 <center><i class='fas fa-trash fa-3x fa-fw'></i></center>
-</div><div class='col-auto align-middle'><h6 class='my-0'><?php echo __( 'Delete your account', 'doliconnect' ); ?></h6><small class='text-muted'><?php echo __( 'Soon, you will be able to delete your account', 'doliconnect' ); ?></small>
+</div>
+		<?php endif; ?>
+<div class='col-auto align-middle'><h6 class='my-0'><?php echo __( 'Delete your account', 'doliconnect' ); ?></h6><small class='text-muted'><?php echo __( 'Soon, you will be able to delete your account', 'doliconnect' ); ?></small>
 </div></div></label></div></li>
 		<?php endif; ?>
     
