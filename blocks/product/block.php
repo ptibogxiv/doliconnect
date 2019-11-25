de712c3dfebfe94367aa6d82407c23e3ccf553de
ptibogxiv <support@ptibogxiv.net>
2019-11-25 14:46:02 +0100

    Update block.php

--- blocks/product/block.php
+++ blocks/product/block.php
@@ -1,16 +1,42 @@
 <?php
 
 function product_block_render( $attributes, $content ) {
-	$args = array();
-	if ( isset( $attributes['request_type'] ) ) {
-		if ( 'export' === $attributes['request_type'] ) {
-			$args['request_type'] = 'export';
-		} elseif ( 'remove' === $attributes['request_type'] ) {
-			$args['request_type'] = 'remove';
-		}
-	}
-	$content = '<div class="gdpr-data-request-block">' . gdrf_data_request_form( $args ) . '</div>';
-	return $content;
+doliconnect_enqueues();
+$content = '<DIV class="card shadow-sm"><DIV class="card-body">';
+if ($attributes['productID']>0) {
+$product = callDoliApi("GET", "/products/".$attributes['productID']."?includestockdata=1", null, dolidelay('product'));
+//$content .= $product;
+$documents = callDoliApi("GET", "/documents?modulepart=product&id=".$product->id, null, dolidelay('product'));
+//$content .= $documents;
+if (defined("DOLIBUG")) {
+$content .= dolibug();
+} else    if ( $product->id>0 && $product->status == 1 ) {
+$content .= '<div class="row">';
+$content .= '<div class="col-12 d-block d-sm-block d-xs-block d-md-none"><center><i class="fa fa-cube fa-fw fa-5x"></i></center>';
+//$content .= wp_get_attachment_image( $attributes['mediaID'], "ptibogxiv_large", "", array( "class" => "img-fluid" ) );
+$content .= '</div>';
+$content .= '<div class="col-md-4 d-none d-md-block"><center><i class="fa fa-cube fa-fw fa-5x"></i></center>';
+//$content .= wp_get_attachment_image( $attributes['mediaID'], "ptibogxiv_square", "", array( "class" => "img-fluid" ) );
+$content .= '</div>';
+$content .= '<div class="col-12 col-md-8"><h5 class="card-title"><b>'.doliproduct($product, 'label')."</b>";
+if ( ! empty(doliconnectid('dolicart')) ) { 
+$content .= " ".doliproductstock($product);
+}
+$content .= "<br><small>".__( 'Reference', 'doliconnect').": ".$product->ref;
+if ( !empty($product->barcode) ) { $content .= " / ".__( 'Barcode', 'doliconnect').": ".$product->barcode; }
+$content .= "</small><p>".doliproduct($product, 'description')."</p>";
+if ( ! empty(doliconnectid('dolicart')) ) { 
+$content .= doliproducttocart($product, null, isset($attributes['showButtonToCart']) ? $attributes['showButtonToCart'] : null, isset($attributes['hideDuration']) ? $attributes['hideDuration'] : null);
+}
+$content .= '</div></div>';
+} else {
+$content .= '<center>'.__( 'Product/Service not in sale', 'doliconnect' ).'</center>';
+} 
+} else {
+$content .= '<center>'.__( 'No product', 'doliconnect' ).'</center>';
+}
+$content .= '</div></div>';
+return $content;
 }
 function product_block_init() {
 	if ( function_exists( 'register_block_type' ) ) {
