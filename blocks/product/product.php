490d9d9c7691288d897d3164a969f83f25b7fbd9
ptibogxiv <support@ptibogxiv.net>
2019-11-25 15:47:10 +0100

    Update product.php

--- blocks/product/product.php
+++ blocks/product/product.php
@@ -1,105 +1,63 @@
 <?php
-/**
- * BLOCK: Product
- *
- * Gutenberg Custom Product Block assets.
- *
- * @since   1.0.0
- * @package OPB
- */
 
-// Exit if accessed directly.
-// if ( ! defined( 'ABSPATH' ) ) {
-// 	exit;
-// }
-
-/**
- * Enqueue the block's assets for the editor.
- *
- * `wp-blocks`: Includes block type registration and related functions.
- * `wp-element`: Includes the WordPress Element abstraction for describing the structure of your blocks.
- * `wp-i18n`: To internationalize the block's text.
- *
- * @since 1.0.0
- */
-
-function doliconnect_product_block() {
-
-	// Scripts.
-	wp_register_script(
-		'doliconnect-product-block-script', // Handle.
-		plugins_url( 'block.js', __FILE__ ), // Block.js: We register the block here.
-		array( 'wp-blocks', 'wp-element', 'wp-i18n' ), // Dependencies, defined above.
-    'beta'
-	);
-
-	// Styles.
-	wp_register_style(
-		'doliconnect-product-block-editor-style', // Handle.
-		plugins_url( 'editor.css', __FILE__ ), // Block editor CSS.
-		array( 'wp-edit-blocks' ), // Dependencies, defined above.
-    'beta'
-	);
-  
-//	wp_register_style(
-//		'doliconnect-product-block-frontend-style', // Handle.
-//		plugins_url( 'style.css', __FILE__ ), // Block editor CSS.
-//		array( 'wp-edit-blocks' ) // Dependency to include the CSS after it.
-//	); 
-
-function doliconnect_product_render_block( $attributes ) {
-
+function doliconnect_product_block_render( $attributes, $content ) {
 doliconnect_enqueues();
-
-$html = '<DIV class="card shadow-sm"><DIV class="card-body">';
-if ($attributes['productID']>0) {
+$content = '<DIV class="card shadow-sm"><DIV class="card-body">';
+if (isset($attributes['productID']) && $attributes['productID']>0) {
 $product = callDoliApi("GET", "/products/".$attributes['productID']."?includestockdata=1", null, dolidelay('product'));
-//$html .= $product;
+//$content .= $product;
 $documents = callDoliApi("GET", "/documents?modulepart=product&id=".$product->id, null, dolidelay('product'));
-//$html .= $documents;
-
+//$content .= $documents;
 if (defined("DOLIBUG")) {
-$html .=dolibug();
+$content .= dolibug();
 } else    if ( $product->id>0 && $product->status == 1 ) {
-$html .= '<div class="row">';
-$html .= '<div class="col-12 d-block d-sm-block d-xs-block d-md-none"><center><i class="fa fa-cube fa-fw fa-5x"></i></center>';
-//$html .= wp_get_attachment_image( $attributes['mediaID'], "ptibogxiv_large", "", array( "class" => "img-fluid" ) );
-$html .= '</div>';
-$html .= '<div class="col-md-4 d-none d-md-block"><center><i class="fa fa-cube fa-fw fa-5x"></i></center>';
-//$html .= wp_get_attachment_image( $attributes['mediaID'], "ptibogxiv_square", "", array( "class" => "img-fluid" ) );
-$html .= '</div>';
-
-$html .= '<div class="col-12 col-md-8"><h5 class="card-title"><b>'.doliproduct($product, 'label')."</b>";
+$content .= '<div class="row">';
+$content .= '<div class="col-12 d-block d-sm-block d-xs-block d-md-none"><center><i class="fa fa-cube fa-fw fa-5x"></i></center>';
+//$content .= wp_get_attachment_image( $attributes['mediaID'], "ptibogxiv_large", "", array( "class" => "img-fluid" ) );
+$content .= '</div>';
+$content .= '<div class="col-md-4 d-none d-md-block"><center><i class="fa fa-cube fa-fw fa-5x"></i></center>';
+//$content .= wp_get_attachment_image( $attributes['mediaID'], "ptibogxiv_square", "", array( "class" => "img-fluid" ) );
+$content .= '</div>';
+$content .= '<div class="col-12 col-md-8"><h5 class="card-title"><b>'.doliproduct($product, 'label')."</b>";
 if ( ! empty(doliconnectid('dolicart')) ) { 
-$html .= " ".doliproductstock($product);
+$content .= " ".doliproductstock($product);
 }
-$html .= "<br><small>".__( 'Reference', 'doliconnect').": ".$product->ref;
-if ( !empty($product->barcode) ) { $html .= " / ".__( 'Barcode', 'doliconnect').": ".$product->barcode; }
-$html .= "</small><p>".doliproduct($product, 'description')."</p>";
-
+$content .= "<br><small>".__( 'Reference', 'doliconnect').": ".$product->ref;
+if ( !empty($product->barcode) ) { $content .= " / ".__( 'Barcode', 'doliconnect').": ".$product->barcode; }
+$content .= "</small><p>".doliproduct($product, 'description')."</p>";
 if ( ! empty(doliconnectid('dolicart')) ) { 
-$html .= doliproducttocart($product, null, isset($attributes['showButtonToCart']) ? $attributes['showButtonToCart'] : null, isset($attributes['hideDuration']) ? $attributes['hideDuration'] : null);
+$content .= doliproducttocart($product, null, isset($attributes['showButtonToCart']) ? $attributes['showButtonToCart'] : null, isset($attributes['hideDuration']) ? $attributes['hideDuration'] : null);
 }
-
-$html .= '</div></div>';
+$content .= '</div></div>';
 } else {
-$html .= '<center>'.__( 'Product/Service not in sale', 'doliconnect' ).'</center>';
+$content .= '<center>'.__( 'Product/Service not in sale', 'doliconnect' ).'</center>';
 } 
+} else {
+$content .= '<center>'.__( 'No product', 'doliconnect' ).'</center>';
 }
-$html .= '</div></div><BR/>';
-return $html;
+$content .= '</div></div>';
+return $content;
 }
-
-	// Here we actually register the block with WP, again using our namespacing
-	// We also specify the editor script to be used in the Gutenberg interface
-	register_block_type( 'doliconnect/product-block', array(
-    'render_callback' => 'doliconnect_product_render_block',
-		'editor_script' => 'doliconnect-product-block-script',
-		'editor_style' => 'doliconnect-product-block-editor-style',
-		'style' => 'doliconnect-product-block-frontend-style',
-	) );
-
-} // End function organic_profile_block().
-
-// Hook: Editor assets.
-add_action( 'init', 'doliconnect_product_block' );
+function doliconnect_product_block_init() {
+	if ( function_exists( 'register_block_type' ) ) {
+		wp_register_script(
+			'product-block',
+			plugins_url( 'block.js', __FILE__ ),
+			array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-components' ),
+      'beta7'
+		);
+		register_block_type(
+			'doliconnect/product-block',
+			array(
+				'editor_script'   => 'product-block',
+				'render_callback' => 'doliconnect_product_block_render',
+				'attributes'      => array(
+					'request_type' => array(
+						'type' => 'string',
+					),
+				),
+			)
+		);
+	}
+}
+add_action( 'init', 'doliconnect_product_block_init' );
