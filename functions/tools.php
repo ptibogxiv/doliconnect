12feb08d468e9496abcbcd7829dde3c700ad4d0b
ptibogxiv <support@ptibogxiv.net>
2019-12-04 17:42:52 +0100

    Update tools.php

--- functions/tools.php
+++ functions/tools.php
@@ -895,13 +895,24 @@ $paymentmethods .= "<p class='text-justify'>";
 $blogname=get_bloginfo('name');
 $paymentmethods .= '<small>'.sprintf( esc_html__( 'By providing your card and confirming this form, you are authorizing %s and Stripe, our payment service provider, to send instructions to the financial institution that issued your card to take payments from your card account in accordance with those instructions. You are entitled to a refund from your financial institution under the terms and conditions of your agreement with your financial institution. A refund must be claimed within 90 days starting from the date on which your card was debited.', 'doliconnect'), $blogname).'</small>';
 $paymentmethods .= "</p>";
-$paymentmethods .= '<p><div class="custom-control custom-radio custom-control-inline">
+$paymentmethods .= '<p>';
+if ( !empty($module) && is_object($object) && isset($object->id) ) {
+$paymentmethods .= '<div class="custom-control custom-radio custom-control-inline">
   <input type="radio" id="cardDefault0" name="cardDefault" value="0"  class="custom-control-input" checked>
-  <label class="custom-control-label" for="cardDefault0">'.__( "Save", 'doliconnect').'</label>
+  <label class="custom-control-label" for="cardDefault0">'.__( "Not save", 'doliconnect').'</label>
 </div>
 <div class="custom-control custom-radio custom-control-inline">
-  <input type="radio" id="cardDefault1" name="cardDefault" value="1" class="custom-control-input">
-  <label class="custom-control-label" for="cardDefault1">'.__( "Save as favourite", 'doliconnect').'</label>
+  <input type="radio" id="cardDefault1" name="cardDefault" value="1"  class="custom-control-input" checked>
+  <label class="custom-control-label" for="cardDefault1">'.__( "Save", 'doliconnect').'</label>
+</div>';} else {
+$paymentmethods .= '<div class="custom-control custom-radio custom-control-inline">
+  <input type="radio" id="cardDefault0" name="cardDefault" value="0"  class="custom-control-input" checked>
+  <label class="custom-control-label" for="cardDefault0">'.__( "Save", 'doliconnect').'</label>
+</div>';
+}
+$paymentmethods .= '<div class="custom-control custom-radio custom-control-inline">
+  <input type="radio" id="cardDefault2" name="cardDefault" value="2" class="custom-control-input">
+  <label class="custom-control-label" for="cardDefault2">'.__( "Save as favourite", 'doliconnect').'</label>
 </div></p>';
 if ( !empty($module) && is_object($object) && isset($object->id) ) {
 $paymentmethods .='<button id="cardPayButton" class="btn btn-danger btn-block" ><b>'.__( 'Pay', 'doliconnect' )." ".doliprice($object, 'ttc', $currency).'</b></button>';
