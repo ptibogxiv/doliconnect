a04fbff34ee6eb2b69c08467d4faf07af3253b75
ptibogxiv <support@ptibogxiv.net>
2019-12-08 19:16:51 +0100

    Update product.php

--- functions/product.php
+++ functions/product.php
@@ -315,9 +315,10 @@ $list .= "<span class='text-muted'>".doliprice($line, 'total_ttc',isset($object-
 }
 }
 
-$cart = "<div class='card'><div class='card-header'>".__( 'Cart', 'doliconnect' )." - ".sprintf( _n( '%s item', '%s items', $qty, 'doliconnect' ), $qty);
-if ( !isset($object->resteapayer) && $object->statut == 0 ) { $cart .= " <small>(<a href='".doliconnecturl('dolicart')."' >".__( 'update', 'doliconnect' )."</a>)</small>"; }
-$cart .= "</div><ul class='list-group list-group-flush'>";
+$cart = "<div class='card'><div class='card-header'>".__( 'Cart', 'doliconnect' )." - ".sprintf( _n( '%s item', '%s items', $qty, 'doliconnect' ), $qty)." <small>(";
+if ( !isset($object->resteapayer) && $object->statut == 0 ) { $cart .= "<a href='".doliconnecturl('dolicart')."' >".__( 'update', 'doliconnect' )."</a>"; }
+else { $cart .= ".__( 'unchangeable, please contact us', 'doliconnect' )."; }
+$cart .= ")</small></div><ul class='list-group list-group-flush'>";
 $cart .= $list;
 
 if ( doliconnector($current_user, 'remise_percent') > 0 && $remise > 0 ) { 
