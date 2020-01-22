bc882d44213809f098e175d36f932f6a5c9317ab
ptibogxiv <support@ptibogxiv.net>
2020-01-22 17:17:47 +0100

    Update templates.php

--- dashboard/templates.php
+++ dashboard/templates.php
@@ -1131,8 +1131,8 @@ print "</div></div>";
 if ( isset($_GET['step']) && $_GET['step'] == 'validation' && isset($_GET['cart']) && wp_verify_nonce( $_GET['cart'], 'valid_dolicart-'.$object->id) && ((doliconnector($current_user, 'fk_order_nb_item') > 0 && $object->statut == 0 && !isset($_GET['module']) ) || ( ($_GET['module'] == 'orders' && $object->billed != 1 ) || ($_GET['module'] == 'invoices' && $object->paye != 1) )) && $object->socid == doliconnector($current_user, 'fk_soc') ) {
 
 //$object = callDoliApi("GET", "/".$module."/".$_GET['id']."?contact_list=0", null, dolidelay('cart', true));
-$pay = callDoliApi("GET", "/doliconnector/".doliconnector($current_user, 'fk_soc')."/pay/".$module."/".$_GET['id'], null, 0);
-print $pay;
+//$pay = callDoliApi("GET", "/doliconnector/".doliconnector($current_user, 'fk_soc')."/pay/".$module."/".$_GET['id'], null, 0);
+//print $pay;
 
 print "<table width='100%' style='border: none'><tr style='border: none'><td width='50px' style='border: none'><div class='fa-3x'>
 <i class='fas fa-shopping-bag fa-fw text-success' data-fa-transform='shrink-3.5' data-fa-mask='fas fa-circle' ></i>
@@ -1163,10 +1163,10 @@ print "' data-fa-transform='shrink-3.5' data-fa-mask='fas fa-circle' ></i>
 </div></td></tr></table><br>"; 
 
 if ( ( !isset($object->id) ) || (doliconnector($current_user, 'fk_soc') != $object->socid) ) {
-$order = callDoliApi("GET", "/".$module."/".$object->id."?contact_list=0", null, 0);
-$dolibarr = callDoliApi("GET", "/doliconnector/".$current_user->ID, null, 0);
-wp_safe_redirect(esc_url(doliconnecturl('dolicart')));
-exit;
+//$order = callDoliApi("GET", "/".$module."/".$object->id."?contact_list=0", null, 0);
+//$dolibarr = callDoliApi("GET", "/doliconnector/".$current_user->ID, null, 0);
+//wp_safe_redirect(esc_url(doliconnecturl('dolicart')));
+//exit;
 }
 
 print "<div class='card shadow-sm' id='cart-form'><div class='card-body'><center><h2>".__( 'Your order has been registered', 'doliconnect' )."</h2>".__( 'Reference', 'doliconnect' ).": ".$object->ref."<br />".__( 'Payment method', 'doliconnect' ).": ".$object->mode_reglement."<br /><br />";
