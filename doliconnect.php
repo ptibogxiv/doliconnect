10d647c35d0e118a688782ea0d38646e3e2d34b7
ptibogxiv <support@ptibogxiv.net>
2020-02-14 16:33:16 +0100

    Update doliconnect.php

--- doliconnect.php
+++ doliconnect.php
@@ -39,7 +39,7 @@ require_once plugin_dir_path(__FILE__).'/blocks/index.php';
 //include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
 define('DOLIBARR_MINIMUM_VERSION', '11.0.0');
-define('DOLIBARR_LEGAL_VERSION', '11.0.0');
+define('DOLIBARR_LEGAL_VERSION', '11.0.1');
 // ********************************************************
 function doliconnecturl($page) {
 global $wpdb;
