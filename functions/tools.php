9a8be6aae118b5003cbd0d5be30f3ce3a1a068da
ptibogxiv <support@ptibogxiv.net>
2020-02-24 18:35:58 +0100

    Update tools.php

--- functions/tools.php
+++ functions/tools.php
@@ -383,7 +383,7 @@ return $bug;
 
 function Doliconnect_MailAlert( $user_login, $user) {
 
-if ( $user->loginmailalert == 'on'  ) { //&& $user->ID != ''.constant("DOLICONNECT_DEMO").''
+if ( $user->loginmailalert == 'on' ) { //&& $user->ID != ''.constant("DOLICONNECT_DEMO").''
 $sitename = get_option('blogname');
 $siteurl = get_option('siteurl');
 $subject = "[$sitename] ".__( 'Connection notification', 'doliconnect');
