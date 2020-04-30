4d49b3f0e4754166619c13c18f3c1d3cade1c1b1
ptibogxiv <support@ptibogxiv.net>
2020-04-30 20:35:06 +0200

    Update tools.php

--- functions/tools.php
+++ functions/tools.php
@@ -2116,7 +2116,7 @@ if ( isset($listpaymentmethods->payment_methods) && $listpaymentmethods->payment
 foreach ( $listpaymentmethods->payment_methods as $method ) {
 $paymentmethods .= '<li id="li-'.$method->id.'" class="nav-item"><a class="nav-link';
 if ( (!empty($thirdparty->mode_reglement_id) && $thirdparty->mode_reglement_id != $method->id && !empty($module) && is_object($object) && isset($object->id)) || (date('Y/n') >= $method->expiration && !empty($object) && !empty($method->expiration)) ) { $paymentmethods .=" disabled "; }
-elseif ( !empty($method->default_source) ) { $paymentmethods .=" active"; }
+elseif ( !empty($method->default_source) && !in_array($method->type, array('PRE','VIR')) ) { $paymentmethods .=" active"; }
 $paymentmethods .= '" data-toggle="pill" href="#nav-tab-'.$method->id.'"><i ';
 if ( $method->type == 'sepa_debit' || $method->type == 'PRE' || $method->type == 'VIR' ) { $paymentmethods .= 'class="fas fa-university fa-fw float-left" style="color:DarkGrey"'; } 
 elseif ( $method->brand == 'visa' ) { $paymentmethods .= 'class="fab fa-cc-visa fa-fw float-left" style="color:#172274"'; }
@@ -2131,7 +2131,7 @@ $paymentmethods .= __( 'Account', 'doliconnect')." ".$method->reference;
 $paymentmethods .= __( 'Card', 'doliconnect').' '.$method->reference;
 }
 //if ( !empty($method->expiration) ) { $paymentmethods .=" - ".date("m/Y", strtotime($method->expiration.'/1')); }
-if ( $method->default_source ) { $paymentmethods .= " <i class='fas fa-star fa-fw' style='color:Gold'></i>"; }
+if ( $method->default_source && !in_array($method->type, array('PRE','VIR')) ) { $paymentmethods .= " <i class='fas fa-star fa-fw' style='color:Gold'></i>"; }
 $paymentmethods .= "<span class='flag-icon flag-icon-".strtolower($method->country)." float-right'></span></a></li>";
 }}
 if (isset($listpaymentmethods->stripe) && in_array('card', $listpaymentmethods->stripe->types) && empty($thirdparty->mode_reglement_id) ) {
@@ -2174,7 +2174,7 @@ $paymentmethods .= "</ul><div class='tab-content'>";
 if ( isset($listpaymentmethods->payment_methods) && $listpaymentmethods->payment_methods != null ) {
 foreach ( $listpaymentmethods->payment_methods as $method ) {
 $paymentmethods .= "<div class='tab-pane fade";
-if ( $method->default_source ) {
+if ( $method->default_source && !in_array($method->type, array('PRE','VIR')) ) {
 $paymentmethods .= " show active"; 
 }
 $paymentmethods .= "' id='nav-tab-".$method->id."'><br>";
@@ -2251,11 +2251,11 @@ $paymentmethods .= '<button type="button" onclick="PaySepaDebitPM(\''.$method->i
 $paymentmethods .= '<button type="button" onclick="PayPM(\''.$method->type.'\')" class="btn btn-danger btn-block">'.__( 'Pay', 'doliconnect')." ".doliprice($object, 'ttc', $currency).'</button>';
 }
 } else {
-if ( !isset($method->default_source)) {
+if ( !isset($method->default_source) && !in_array($method->type, array('PRE','VIR')) ) {
 $paymentmethods .= "<button type='button' id='defaultbtn_".$method->id."' name='default_payment_method' value='default_payment_method' class='btn btn-light'";
 $paymentmethods .= "title='".__( 'Favourite', 'doliconnect')."'><i class='fas fa-star fa-fw' style='color:Gold'></i> ".__( "Favourite", 'doliconnect')."</button>";
 }
-if ( (!isset($method->default_source) && $countPM > 1) || $countPM == 1 ) { 
+if ( (!isset($method->default_source) && $countPM > 1) || $countPM == 1  || in_array($method->type, array('PRE','VIR')) ) { 
 $paymentmethods .= "<button type='button' id='deletebtn_".$method->id."' name='delete_payment_method' value='delete_payment_method' class='btn btn-light'";
 $paymentmethods .= "title='".__( 'Delete', 'doliconnect')."'><i class='fas fa-trash fa-fw' style='color:Red'></i> ".__( 'Delete', 'doliconnect').'</button>';
 }}
