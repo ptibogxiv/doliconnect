f66d60edeaafdca0479d3857a672a5aab7e303ba
ptibogxiv <support@ptibogxiv.net>
2020-05-01 13:56:17 +0200

    Update product.php

--- functions/product.php
+++ functions/product.php
@@ -37,15 +37,18 @@ return numfmt_format_currency($fmt, $montant, $currency);//.$decimal
 
 function doliproductstock($product, $refresh = false) {
 
-$stock = '<script>
-jQuery(document).ready(function(){
-      $("#popover-'.$product->id.'").popover({
-      placement : "auto",
-      delay: { "show": 150, "hide": 150 },
-      trigger : "focus",
-      html : true
-      })
-});</script>';
+$stock = "<script>";
+$stock .= "(function ($) {
+$(document).ready(function(){
+$('#popover-".$product->id."').popover({
+placement : 'auto',
+delay: { 'show': 150, 'hide': 150 },
+trigger : 'focus',
+html : true
+})
+});
+})(jQuery);";
+$stock .= "</script>";
 
 if ( ! is_object($product) || empty(doliconst('MAIN_MODULE_STOCK')) || ($product->type != '0' && empty(doliconst('STOCK_SUPPORTS_SERVICES')) )) {
 $stock .= "<a tabindex='0' id='popover-".$product->id."' class='badge badge-pill badge-success text-white' data-container='body' data-toggle='popover' data-trigger='focus' title='".__( 'Available immediately', 'doliconnect')."' data-content='".sprintf( __( 'This item is in stock and can be send immediately. %s', 'doliconnect'), '')."'><i class='fas fa-warehouse'></i> ".__( 'Available immediately', 'doliconnect').'</a>';
