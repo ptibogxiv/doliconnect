5d7d55ab03bbe94b175cf0c893d86306e6ed7d1b
ptibogxiv <support@ptibogxiv.net>
2020-09-11 18:02:59 +0200

    Update cron.php

--- functions/cron.php
+++ functions/cron.php
@@ -1,11 +1,11 @@
 <?php
 
-
 add_action( 'doliconnect_cron_hook', 'doliconnect_cron_process' );
 
 function doliconnect_cron_process($refresh = false) {
 
-$product = array();
+$products = array();
+$categories = array();
 
 if (get_option('dolicartnewlist') != 'none') {
 $date = new DateTime(); 
@@ -16,8 +16,8 @@ $lastdate = $date->format('Y-m-d');
 $requestp = "/products?sortfield=t.datec&sortorder=DESC&sqlfilters=(t.datec%3A%3E%3A'".$lastdate."')%20AND%20(t.tosell%3A%3D%3A1)";
 $listproduct = callDoliApi("GET", $requestp, null, dolidelay('product', $refresh));
 if ( !isset($listproduct->error) && $listproduct != null ) {
-foreach ($listproduct as $produit) {
-$product[$produit->id] = $produit->id;
+foreach ($listproduct as $product) {
+$products[$product->id] = $product->id;
 }}
 }
 
@@ -28,49 +28,45 @@ $lastdate = $date->format('Y-m-d');
 $requestp = "/discountprice?sortfield=t.rowid&sortorder=ASC&sqlfilters=(t.date_begin%3A%3C%3D%3A'".$lastdate."')%20AND%20(t.date_end%3A%3E%3D%3A'".$lastdate."')%20AND%20(d.tosell%3A%3D%3A1)";
 $listproduct = callDoliApi("GET", $requestp, null, dolidelay('product', $refresh));
 if ( !isset($listproduct->error) && $listproduct != null ) {
-foreach ($listproduct as $produit) {
-$product[$produit->fk_product] = $produit->fk_product;
+foreach ($listproduct as $product) {
+$products[$product->fk_product] = $product->fk_product;
 }}
 }
 
-$shop = doliconst("DOLICONNECT_CATSHOP", $refresh);
+$shop = doliconst("DOLICONNECT_CATSHOP");
 $request = "/categories/".esc_attr($shop)."?include_childs=true";
 $resultatsc = callDoliApi("GET", $request, null, dolidelay('product', $refresh));
-
-foreach ($resultatsc->childs as $categorie) {
-
-$requestp = "/products?sortfield=t.label&sortorder=ASC&category=".$categorie->id."&sqlfilters=(t.tosell=1)";
-$listproduct = callDoliApi("GET", $requestp, null, dolidelay('product', $refresh));
-
-if ( isset($_GET['category']) && $categorie->id == $_GET['category'] ) {
-
-$request = "/categories/".esc_attr(isset($_GET["category"]) ? $_GET["category"] : $_GET["subcategory"])."?include_childs=true";
-$resultatsc = callDoliApi("GET", $request, null, dolidelay('product', $refresh));
-
 if ( !isset($resultatsc->error) && $resultatsc != null ) {
 foreach ($resultatsc->childs as $categorie) {
-$requestp = "/products?sortfield=t.label&sortorder=ASC&category=".$categorie->id."&sqlfilters=(t.tosell=1)";
-$listproduct = callDoliApi("GET", $requestp, null, dolidelay('product', $refresh));
+$categories[$categorie->id] = $categorie->id;
+}}
+
+foreach ($categories as $id => $categorie) {
+$request = "/categories/".$id."?include_childs=true";
+$resultatsc = callDoliApi("GET", $request, null, dolidelay('product', $refresh));
+if ( !isset($resultatsc->error) && $resultatsc != null ) {
+foreach ($resultatsc->childs as $categorie) {
+$categories[$categorie->id] = $categorie->id;
 }}
 }
-} 
+
+
+foreach ($categories as $id => $categorie) {
+$requestp = "/products?sortfield=t.label&sortorder=ASC&category=".$id."&sqlfilters=(t.tosell=1)";
+$listproduct = callDoliApi("GET", $requestp, null, dolidelay('product', $refresh));
+if ( !isset($listproduct->error) && $listproduct != null ) {
+foreach ($listproduct as $product) {
+$products[$product->id] = $product->id;
+}}
+}
 
 $includestock = 0;
 if ( ! empty(doliconnectid('dolicart')) ) {
 $includestock = 1;
 }  
-foreach ($product as $id => $product) {
+foreach ($products as $id => $product) {
 $product = callDoliApi("GET", "/products/".$id."?includestockdata=".$includestock."&includesubproducts=true", null, dolidelay('product', $refresh));
 }
 
-    //error_log('Mi evento se ejecutó: '.Date("h:i:sa"));
-    $recepients="support@ptibogxiv.net";
-    $subject="Hello from your Cron Job";
-    $message="This is a test mail sent by WordPress automatically as per Your schedule.";
-    //let’s send it
-    //wp_mail($recepients,$subject,$message);
 }
-
-
-
 ?>
