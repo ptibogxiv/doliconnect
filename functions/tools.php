db86a46dd9e778e1f22b388ee4abb48997c4796d
ptibogxiv <support@ptibogxiv.net>
2021-03-14 17:56:09 +0100

    Update tools.php

--- functions/tools.php
+++ functions/tools.php
@@ -3007,29 +3007,27 @@ print '" required><label for="user_pass"><i class="fas fa-key fa-fw"></i> '.__( 
 
 do_action( 'login_form' );
 
-print '<div class="form-check">
+print '<div class="form-check float-start">
   <input class="form-check-input" type="checkbox" name="rememberme" value="forever" id="rememberme" checked>
   <label class="form-check-label" for="rememberme">'.__( 'Remember me', 'doliconnect').'</label>
 </div>';
 
-if ( get_site_option('doliconnect_mode') == 'one' && function_exists('switch_to_blog') ) {
-switch_to_blog(1);
-} 
-
-if ((!is_multisite() && get_option( 'users_can_register' )) || ((!is_multisite() && get_option( 'dolicustsupp_can_register' )) || ((get_option( 'dolicustsupp_can_register' ) || get_option('users_can_register') == '1') && (get_site_option( 'registration' ) == 'user' || get_site_option( 'registration' ) == 'all')))) {
-print "<a class='float-start' href='".wp_registration_url(get_permalink())."' role='button' title='".__( 'Create an account', 'doliconnect')."'><small>".__( 'Create an account', 'doliconnect')."</small></a>";
-}
 print "<a class='float-end' href='".wp_lostpassword_url(get_permalink())."' role='button' title='".__( 'Forgot password?', 'doliconnect')."'><small>".__( 'Forgot password?', 'doliconnect')."</small></a>"; 
-
-if (get_site_option('doliconnect_mode')=='one') {
-restore_current_blog();
-}
 
 print "<input type='hidden' value='$redirect_to' name='redirect_to'></div>";
 
 print "".doliloading('loginmodal-form');
 
 print '</div><div id="Footerloginmodal-form" class="modal-footer">';
+if ( get_site_option('doliconnect_mode') == 'one' && function_exists('switch_to_blog') ) {
+switch_to_blog(1);
+} 
+if ((!is_multisite() && get_option( 'users_can_register' )) || ((!is_multisite() && get_option( 'dolicustsupp_can_register' )) || ((get_option( 'dolicustsupp_can_register' ) || get_option('users_can_register') == '1') && (get_site_option( 'registration' ) == 'user' || get_site_option( 'registration' ) == 'all')))) {
+print "<a class='btn btn-secondary' href='".wp_registration_url(get_permalink())."' role='button' title='".__( 'Create an account', 'doliconnect')."'><small>".__( 'Create an account', 'doliconnect')."</small></a>";
+}
+if (get_site_option('doliconnect_mode')=='one') {
+restore_current_blog();
+}
 print '<div class="d-grid gap-2"><button class="btn btn-outline-secondary" type="submit" value="submit">'.__( 'Sign in', 'doliconnect').'</button></div>';
 print '</form></div>';
 print '</div></div></div>';
