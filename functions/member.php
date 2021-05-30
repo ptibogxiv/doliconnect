00829fc75115f5a85ca7a146c7d939f35e53e002
ptibogxiv <support@ptibogxiv.net>
2021-05-30 21:09:07 +0200

    wip

--- functions/member.php
+++ functions/member.php
@@ -209,24 +209,22 @@ print doliloading('subscription');
 
 print "</div><div id='subscription-footer' class='modal-footer'><small class='text-justify'>".__( 'Note: the admins reserve the right to change your membership in relation to your personal situation. A validation of the membership may be necessary depending on the cases.', 'doliconnect')."</small></div></div></div></div>";
 
-print '<div class="modal fade" id="PaySubscription" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Understood</button>
-      </div>
-    </div>
-  </div>
-</div>';
+$request= "/adherentsplus/type/".$adherent->typeid;
+$adherenttype = callDoliApi("GET", $request, null, dolidelay('member', esc_attr(isset($_GET["refresh"]) ? $_GET["refresh"] : null)));
 
+print '<div class="modal fade" id="PaySubscription" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable"><div class="modal-content"><div class="modal-header">
+<h5 class="modal-title" id="staticBackdropLabel">'.__( 'Subscription', 'doliconnect').'</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+</div><div class="modal-body">
+<h6>'.__( 'This subscription', 'doliconnect').'</h6>
+'.__( 'Price:', 'doliconnect').' '.doliprice($adherenttype->price_prorata).'<br>
+'.__( 'From', 'doliconnect').' '.wp_date('d/m/Y', $adherenttype->date_begin).' '.__( 'until', 'doliconnect').' '.wp_date('d/m/Y', $adherenttype->date_end).'
+<h6>'.__( 'Next subscription', 'doliconnect').'</h6>
+'.__( 'Price:', 'doliconnect').' '.doliprice($adherenttype->price).'<br>
+'.__( 'From', 'doliconnect').' '.wp_date('d/m/Y', $adherenttype->date_nextbegin).' '.__( 'until', 'doliconnect').' '.wp_date('d/m/Y', $adherenttype->date_nextend).'
+</div><div class="modal-footer">
+<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button><button type="button" class="btn btn-primary">Understood</button>
+</div></div></div></div>';
 }}
 add_action( 'wp_footer', 'doliconnect_membership_modal');
 ?>
