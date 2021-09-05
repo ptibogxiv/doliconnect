dc9ead0756c1dffa3e37b8ffb7821d3f01e5d3c6
ptibogxiv <support@ptibogxiv.net>
2021-09-05 23:14:46 +0200

    wip

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -20,7 +20,7 @@ $versionbase = '5.1.0';
 $version=$versionbase; 
 } else {
 $css='bootswatch/'.get_theme_mod( 'ptibogxivtheme_css').'/';
-$version='5.0.2';  
+$version='5.1.0';  
 }
 
 if (!empty(get_theme_mod( 'ptibogxivtheme_css')) && $version != $versionbase && empty(get_option('doliconnectbeta'))) {
