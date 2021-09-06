f9564f399439c0ccc9cdf58aa2bc8132635b895c
ptibogxiv <support@ptibogxiv.net>
2021-09-06 14:52:51 +0200

    Update data-request.php

--- functions/data-request.php
+++ functions/data-request.php
@@ -459,14 +459,12 @@ add_action('wp_ajax_nopriv_dolimember_request', 'dolimember_request');
 
 function dolimember_request(){
 global $current_user;
-
-
 		
 if ( wp_verify_nonce( trim($_POST['dolimember-nonce']), 'dolimember-nonce' ) ) {
 //if ( $_POST['cartaction'] == 'addtocart') {
-$productadhesion = doliconst("ADHERENT_PRODUCT_ID_FOR_SUBSCRIPTIONS", dolidelay('constante', esc_attr(isset($_GET["refresh"]) ? $_GET["refresh"] : null)));
+$productadhesion = doliconst("ADHERENT_PRODUCT_ID_FOR_SUBSCRIPTIONS", dolidelay('constante'));
 $requesta= "/adherentsplus/type/1";//.$adherent->typeid;
-$adherenttype = callDoliApi("GET", $requesta, null, dolidelay('member', esc_attr(isset($_GET["refresh"]) ? $_GET["refresh"] : null)));
+$adherenttype = callDoliApi("GET", $requesta, null, dolidelay('member'));
 $result = doliaddtocart($productadhesion, 1, $adherenttype->price_prorata, null, $adherenttype->date_begin, $adherenttype->date_end);
 //$result = doliaddtocart(trim($_POST['product-add-id']), trim($_POST['product-add-qty']), trim($_POST['product-add-price']), trim($_POST['product-add-remise_percent']), isset($_POST['product-add-timestamp_start'])?trim($_POST['product-add-timestamp_start']):null, isset($_POST['product-add-timestamp_end'])?trim($_POST['product-add-timestamp_end']):null);
 if ($result >= 0) {
