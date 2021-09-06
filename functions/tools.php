ff259c36f6e3c7a70fa0feaa845f04e80e348c9f
ptibogxiv <support@ptibogxiv.net>
2021-09-06 11:46:58 +0200

    wip

--- functions/tools.php
+++ functions/tools.php
@@ -801,14 +801,9 @@ print '<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasCartDol
       Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
     </div>
     <div class="dropdown mt-3">
-      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
-        Dropdown button
-      </button>
-      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-      </ul>
+<div class="d-grid gap-2">
+  <a href="'.esc_url(doliconnecturl('dolicart')).'" class="btn btn-primary" type="button">'.__( 'Go to basket', 'doliconnect').'</a>
+</div>
     </div>
   </div>
 </div>';
