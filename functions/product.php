ef32bf2656feaaaf50df981adb765cf810d90d9e
ptibogxiv <support@ptibogxiv.net>
2021-09-12 13:39:08 +0200

    Update product.php

--- functions/product.php
+++ functions/product.php
@@ -116,7 +116,35 @@ $ln .= '</ul><div class="dropdown mt-3">
 <div class="d-grid gap-2">
   <a href="'.esc_url(doliconnecturl('dolicart')).'" class="btn btn-primary" type="button">'.__( 'Go to basket', 'doliconnect').'</a>
 </div>
-    </div>';
+    </div><table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>';
 return $ln;
 } else {
 return '<center class="p-3 text-muted">'.__( 'Your basket is empty', 'doliconnect').'</center>';
