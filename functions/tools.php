60660eb899338f8e38305fceb70ab4bb2ac758a4
ptibogxiv <support@ptibogxiv.net>
2021-10-02 18:34:27 +0200

    wip

--- functions/tools.php
+++ functions/tools.php
@@ -2589,7 +2589,7 @@ if ( ! function_exists('dolikiosk') || ( function_exists('dolikiosk') && empty(d
 print socialconnect ( get_permalink() );
 }
 
-if ( function_exists('secupress_get_module_option') && !empty(get_site_option('doliconnect_login')) && secupress_get_module_option('move-login_slug-login', null, 'users-login' )) {
+if ( function_exists('secupress_get_module_option') && isset(get_site_option('doliconnect_login')) && secupress_get_module_option('move-login_slug-login', null, 'users-login' )) {
 $login_url = site_url()."/".secupress_get_module_option('move-login_slug-login', null, 'users-login' ); 
 } elseif (get_site_option('doliconnect_login')) {
 $login_url = site_url()."/".get_site_option('doliconnect_login');
