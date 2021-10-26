31376f70dbc449f4827e2e5a9eeb663024f382ce
ptibogxiv <support@ptibogxiv.net>
2021-10-26 21:14:22 +0200

    upgrade flat icon css

--- includes/flag-icon-css/assets/docs.js
+++ includes/flag-icon-css/assets/docs.js
@@ -1,13 +1,64 @@
-window.onload = function () {
-  $('.all-flags .flag-icon-background').click(function(event){
-    var flag = $(event.currentTarget).attr('title');
-    var w = 640;
-    var h = 480;
-    var left = (screen.width / 2) - (w / 2);
-    var top = (screen.height / 2) - (h / 2);
+const loadJSON = (path, callback) => {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType('application/json');
+  xobj.open('GET', path, true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == '200') {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+};
 
-    if (flag) {
-      window.open('flags/4x3/' + flag + '.svg', 'flag-4x3', 'width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+const addFlag = (country, rowDiv) => {
+  const colDiv = document.createElement('div');
+  colDiv.classList.add('col-xl-2');
+  colDiv.classList.add('col-lg-3');
+  colDiv.classList.add('col-md-4');
+  colDiv.classList.add('col-6');
+  colDiv.id = country.code;
+  const flagDiv = document.createElement('div');
+  flagDiv.classList.add('flag');
+
+  // Code
+  const codeSpan = document.createElement('span');
+  codeSpan.classList.add('flag-code');
+  const code = document.createTextNode(country.code);
+  codeSpan.appendChild(code);
+
+  //Country
+  const countryDiv = document.createElement('div');
+  countryDiv.classList.add('flag-country');
+  countryDiv.classList.add('no-wrap');
+  countryDiv.title = country.name;
+  const countrySpan = document.createElement('span');
+  const countryName = document.createTextNode(country.name);
+  countrySpan.appendChild(countryName)
+  countryDiv.appendChild(codeSpan);
+  countryDiv.appendChild(countrySpan);
+  const flagImg = document.createElement('img');
+  flagImg.classList.add('flag-img');
+  flagImg.src = country.flag_4x3;
+  flagImg.alt = `Flag of ${country.name}`;
+
+  colDiv.appendChild(flagDiv);
+  flagDiv.appendChild(countryDiv);
+  flagDiv.appendChild(flagImg);
+  rowDiv.appendChild(colDiv);
+};
+
+window.onload = function () {
+  const isoFlagsRow = document.getElementById('iso-flags');
+  const nonIsoFlagsRow = document.getElementById('non-iso-flags');
+  loadJSON('country.json', response => {
+    const countries = JSON.parse(response);
+    for (country of countries) {
+      console.log(country);
+      if (country.iso) {
+        addFlag(country, isoFlagsRow);
+      } else {
+        addFlag(country, nonIsoFlagsRow);
+      }
     }
   });
-}
+};
