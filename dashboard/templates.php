31bc1770a24f6220ca60059bef94131ab78a490a
ptibogxiv <support@ptibogxiv.net>
2022-01-16 20:03:52 +0100

    Update templates.php

--- dashboard/templates.php
+++ dashboard/templates.php
@@ -647,19 +647,15 @@ function dolifaq_display($content) {
     //print var_dump($faq);
     
     print '<div class="card"><div class="card-header">'.__( 'Knowledge base', 'doliconnect').'</div>';
-    print '<div class="accordion accordion-flush" id="accordionFlushExample">';
+    print '<div class="accordion accordion-flush" id="accordionDolifaq">';
     if ( !isset( $listfaq->error ) && $listfaq != null ) {
         foreach ( $listfaq as $postfaq ) { 
-    print '<div class="accordion-item">
-      <h2 class="accordion-header" id="flush-headingOne">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-          Accordion Item #1
-        </button>
-      </h2>
-      <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first items accordion body.</div>
-      </div>
-    </div>';
+    print '<div class="accordion-item"><h2 class="accordion-header" id="flush-headingOne">';
+    print '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">';
+    print $postfaq->question;
+    print '</button></h2>
+    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionDolifaq">
+    <div class="accordion-body">'.$postfaq->answer.'</div></div></div>';
     }}
   print '</div>';
 
