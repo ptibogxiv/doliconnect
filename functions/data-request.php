f5aa6c434608dd66557db4e132c9a4af5e9d9cbf
ptibogxiv <support@ptibogxiv.net>
2022-01-26 17:42:50 +0100

    Update data-request.php

--- functions/data-request.php
+++ functions/data-request.php
@@ -288,10 +288,12 @@ function dolicontact_request(){
 			
 		} else {
 			wp_send_json_error( dolialert('danger', join( '<br />', $ContactError )));
+			die();	
 		}
 
 		if ( !is_wp_error( $emailSent )) {
 			wp_send_json_success( dolialert('success', __( 'Your message is successful send!', 'doliconnect')));
+			die();	
 		} else {
 			wp_send_json_error( dolialert('error', sprintf(__('Sending message fails for the following reason: %s. Please contact us for help.', 'doliconnect'), $emailSent->get_error_message()) ));	
 			die();	
