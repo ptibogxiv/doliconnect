08b14106f52c1a8fd8de9582b6b12a83d0c4d694
ptibogxiv <support@ptibogxiv.net>
2022-10-27 13:24:47 +0200

    Update product.php

--- functions/product.php
+++ functions/product.php
@@ -346,23 +346,23 @@ $button .= '$(function() {
               },
           }).done(function(response) {
               if (response.success) { 
-                //console.log("updated qty of '.$product->id.' to " + response.data.newqty);
-                if (document.getElementById("qty-prod-'.$product->id.'") && response.data.newqty) {
+                console.log(response.data.message);
+                if (document.getElementById("qty-prod-'.$product->id.'")) {
                   document.getElementById("qty-prod-'.$product->id.'").value = response.data.newqty;
                 }
-                if (document.getElementById("DoliHeaderCartItems") && response.data.items) {
+                if (document.getElementById("DoliHeaderCartItems")) {
                   document.getElementById("DoliHeaderCartItems").innerHTML = response.data.items;
                 }
-                if (document.getElementById("DoliFooterCartItems") && response.data.items) {  
+                if (document.getElementById("DoliFooterCartItems")) {  
                   document.getElementById("DoliFooterCartItems").innerHTML = response.data.items;
                 }
-                if (document.getElementById("DoliCartItemsList") && response.data.list) {  
+                if (document.getElementById("DoliCartItemsList")) {  
                   document.getElementById("DoliCartItemsList").innerHTML = response.data.list;
                 }
-                if (document.getElementById("DoliWidgetCartItems") && response.data.items) {
+                if (document.getElementById("DoliWidgetCartItems")) {
                   document.getElementById("DoliWidgetCartItems").innerHTML = response.data.items;      
                 }
-                if (document.getElementById("message-dolicart") && response.data.message) {
+                if (document.getElementById("message-dolicart")) {
                   document.getElementById("message-dolicart").innerHTML = response.data.message;      
                 }   
               } else {
