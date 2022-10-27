0775190eef64918191d1d50533726ade0780009e
ptibogxiv <support@ptibogxiv.net>
2022-10-27 18:37:03 +0200

    Update product.php

--- functions/product.php
+++ functions/product.php
@@ -176,7 +176,7 @@ $ln = '<table class="table table-hover table-sm"><thead><tr>
 <th scope="col" width="40px">'.__( 'Qty', 'doliconnect').'</th><th scope="col">'.__( 'Item', 'doliconnect').'</th></tr></thead><tbody>';
 foreach ( $order->lines as $line ) { 
 $ln .= '<tr><td scope="row">'.$line->qty.'</td><td><small>'.doliproduct($line, 'product_label');
-if ( !empty(get_option('doliconnectbeta')) ) $ln .= '<div class="float-end"><i class="fa-solid fa-trash-can"></i></div>';
+if ( !empty(get_option('doliconnectbeta')) ) $ln .= '<div class="float-end"><a type="button"><i class="fa-solid fa-trash-can"></i></a></div>';
 $ln .= '</small></td></tr>';
 }
 $ln .= '</tbody><tfoot><tr><th colspan="2" class="table-active">'.__( 'Total to be paid', 'doliconnect').' '.doliprice($order, 'ttc', isset($order->multicurrency_code) ? $order->multicurrency_code : null).'</th></tr></tfoot></table><div class="dropdown mt-3">
