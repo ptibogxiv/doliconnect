785f925f511a9f0bdcf62e1c9068156120e64d52
ptibogxiv <support@ptibogxiv.net>
2023-01-14 17:04:34 +0100

    wip

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -2781,40 +2781,8 @@ function generate_license($suffix = null) {
     return $license_string;
 }
 
-print '<script type="text/javascript">';
-print 'jQuery(document).ready(function($) {
-    document.querySelector("#buttonmodaltest").addEventListener("click", function() {
-        //var m1 = $(makeModal("Something header","Something body","Something footer"));
-        if (document.getElementById("doliModalDiv")) {
-            document.getElementById("doliModalDiv").innerHTML = makeModal("Something header","Something body","Something footer");      
-        }
-        $("#doliModalTest").modal("show");
+print doliModalButton('Test');
 
-    $("#doliModalTest").on("hidden.bs.modal", function () {
-        document.getElementById("doliModal").innerHTML = "";
-    });
-
-    }, false);
- 
-    function makeModal(header, body, footer) {
-        return `<div id="doliModalTest" class="modal fade" role="dialog" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" style="display: none">
-        <div class="modal-dialog modal-fullscreen-md-down modal-dialog-centered modal-dialog-scrollable" role="document">
-      
-          <!-- Modal content-->
-          <div class="modal-content"><div class="modal-header"><h4 class="modal-title">${header}</h4>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">${body}</div>
-            <div class="modal-footer">${footer}</div>
-          </div>
-      
-        </div>
-      </div>`;
-    }
-
-});';
-print '</script>';
-print '<button id="buttonmodaltest" class="btn btn-primary" type="button">Click here</button>';
 //print generate_license();	 
 
 
--- functions/tools.php
+++ functions/tools.php
@@ -3004,6 +3004,41 @@ global $current_user;
 
 }
 
+function doliModalButton($title) {
+
+  $button = '<script type="text/javascript">';
+  $button .= 'jQuery(document).ready(function($) {
+    document.querySelector("#buttonmodaltest").addEventListener("click", function() {
+      if (document.getElementById("doliModalDiv")) {
+        document.getElementById("doliModalDiv").innerHTML = makeModal("'.$title.'","Something body","Something footer");      
+      }
+      $("#doliModalTest").modal("show");
+      $("#doliModalTest").on("hidden.bs.modal", function () {
+        $("#doliModalTest").modal("dispose");
+        document.getElementById("doliModalDiv").innerHTML = "";
+      });
+      }, false);
+   
+      function makeModal(header, body, footer) {
+        return `<div id="doliModalTest" class="modal fade" role="dialog" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" style="display: none">
+          <div class="modal-dialog modal-fullscreen-md-down modal-dialog-centered modal-dialog-scrollable" role="document">
+            <div class="modal-content"><div class="modal-header"><h4 class="modal-title">${header}</h4>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">${body}</div>
+              <div class="modal-footer">${footer}</div>
+            </div>
+          </div>
+        </div>`;
+      }
+  
+  });';
+  $button .= '</script>';
+  $button .= '<button id="buttonmodaltest" class="btn btn-primary" type="button">'.$title.'</button>';
+
+  return $button;
+}
+
 function doliconnect_modalform() {
 global $current_user;
 $year = strftime("%Y", current_time( 'timestamp', 1));
