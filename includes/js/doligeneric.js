218a47951e7a457bed001ee07dece8fc8ac17425
ptibogxiv <support@ptibogxiv.net>
2023-01-28 14:42:12 +0100

    Update doligeneric.js

--- includes/js/doligeneric.js
+++ includes/js/doligeneric.js
@@ -5,8 +5,8 @@
 
 		var form = document.getElementById('loginmodal-form');
 		form.addEventListener('submit', function(event) { 
+		jQuery("#DoliconnectLoadingModal").modal("show");
 		jQuery(window).scrollTop(0);
-		console.log("submit");
 		form.submit();
 		});
 
