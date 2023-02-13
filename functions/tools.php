964ebe6a458f1dec6db1975df75d3bed9649dbc0
ptibogxiv <support@ptibogxiv.net>
2023-02-13 16:45:34 +0100

    Update tools.php

--- functions/tools.php
+++ functions/tools.php
@@ -1038,19 +1038,20 @@ $monnaie = doliconst("MAIN_MONNAIE", dolidelay('constante', esc_attr(isset($_GET
 $testvalue='1.99';
 $cur = (!empty($object->multicurrency_code) ? $object->multicurrency_code : $monnaie );
 if ( !doliCheckModules('multicurrency', esc_attr(isset($_GET["refresh"]) ? $_GET["refresh"] : null)) || !doliversion('11.0.0') ) { 
-$doliuser .= '<input type="text" class="form-control" id="'.$idobject.'[multicurrency_code]" value="'.$cur." / ".doliprice($testvalue, null, $cur).'" readonly>
-<label for="'.$idobject.'[multicurrency_code]">'.__( 'Default currency', 'doliconnect').'</label>';
-} else {
-$doliuser .= '<select class="form-select" id="'.$idobject.'[multicurrency_code]" name="'.$idobject.'[multicurrency_code]" aria-label="'.__( 'Default currency', 'doliconnect').'">';
-if ( !isset( $currencies->error ) && $currencies != null && doliCheckModules('multicurrency', esc_attr(isset($_GET["refresh"]) ? $_GET["refresh"] : null)) && doliversion('11.0.0')) {
-foreach ( $currencies as $currency ) { 
-$doliuser .= "<option value='".$currency->code_iso."' ";
-if ( $currency->code_iso == $object->multicurrency_code ) { $doliuser .= " selected"; }
-$doliuser .= ">".$currency->code_iso." / ".doliprice(1.99*$currency->rate, null, $currency->code_iso)."</option>";
-}} else {
-$doliuser .= "<option value='".$cur."' selected>".$cur." / ".doliprice($testvalue, null, $cur)."</option>";
-}
-$doliuser .= '</select><label for="'.$idobject.'[multicurrency_code]">'.__( 'Default currency', 'doliconnect').'</label>';
+  $doliuser .= '<input type="text" class="form-control" id="'.$idobject.'[multicurrency_code]" value="'.$cur." / ".doliprice($testvalue, null, $cur).'" readonly>';
+  $doliuser .= '<label for="'.$idobject.'[multicurrency_code]">'.__( 'Default currency', 'doliconnect').'</label>';
+} else {  
+  $doliuser .= '<select class="form-select" id="'.$idobject.'[multicurrency_code]" name="'.$idobject.'[multicurrency_code]" aria-label="'.__( 'Default currency', 'doliconnect').'">';
+  if ( !isset( $currencies->error ) && $currencies != null && doliCheckModules('multicurrency', esc_attr(isset($_GET["refresh"]) ? $_GET["refresh"] : null)) && doliversion('11.0.0')) {
+    foreach ( $currencies as $currency ) { 
+      $doliuser .= "<option value='".$currency->code_iso."' ";
+      if ( $currency->code_iso == $object->multicurrency_code ) { $doliuser .= " selected"; }
+      $doliuser .= ">".$currency->code_iso." / ".doliprice(1.99*$currency->rate, null, $currency->code_iso)."</option>";
+    }
+  } else {
+    $doliuser .= "<option value='".$cur."' selected>".$cur." / ".doliprice($testvalue, null, $cur)."</option>";
+  }
+  $doliuser .= '</select><label for="'.$idobject.'[multicurrency_code]">'.__( 'Default currency', 'doliconnect').'</label>';
 }
 $doliuser .= '</div></div>';
 }
