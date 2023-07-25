c829ea8793ecbd16d5747ced3b449ad75f572df9
ptibogxiv <support@ptibogxiv.net>
2023-07-25 16:21:23 +0200

    Update data-request.php

--- functions/data-request.php
+++ functions/data-request.php
@@ -1223,9 +1223,9 @@ global $current_user;
 		wp_send_json_success($response);
 		die();
 	} elseif ( wp_verify_nonce( trim($_POST['dolimodal-nonce']), 'dolimodal-nonce' ) && isset($_POST['case']) && $_POST['case'] == "doliCart" ) {
-		$data = "data:application/pdf;base64,".$_POST['value2'];
+		$object = callDoliApi("GET", "/orders/".doliconnector($current_user, 'fk_order')."?contact_list=0", null, dolidelay('order'));
 		$modal['header'] = __( 'Cart', 'doliconnect');
-		$modal['body'] = null;
+		$modal['body'] = doliline($object, false, false);
 		$modal['footer'] = '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">'.__( "Continue shopping", "doliconnect").'</button> <a class="btn btn-primary" role="button" href="'.esc_url(doliconnecturl('dolicart')).'" >'.__( 'Finalize the order', 'doliconnect').'</a>';
 		$response['js'] = null;
 		$response['modal'] = doliModalTemplate($_POST['id'], $modal['header'], $modal['body'], $modal['footer'], 'modal-lg', null, 'p-0');
