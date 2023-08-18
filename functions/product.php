0c720eed633b2ac1e0534f34507d0ced3adcd110
ptibogxiv <support@ptibogxiv.net>
2023-08-18 11:16:12 +0200

    Update product.php

--- functions/product.php
+++ functions/product.php
@@ -83,7 +83,7 @@ global $current_user;
   }
   if ( isset($order->lines) && $order->lines != null ) {
     foreach ($order->lines as $line) {
-      $line_array_options = (array) $line->array_options;
+      if (is_object($array_options)) $line_array_options = (object) $line->array_options;
       if ($line->fk_product == $product->id && $line_array_options == $array_options) {
         //$button = var_dump($line);
         $mstock['qty'] = $line->qty;
