6cd7f95e9573073b7bd03e6c13c222997d94859a
ptibogxiv <support@ptibogxiv.net>
2023-08-20 21:22:05 +0200

    Update tools.php

--- functions/tools.php
+++ functions/tools.php
@@ -1633,8 +1633,12 @@ return __('Transporter by default', 'doliconnect');
 function doliWishlist($thirdpartyid, $productid, $refresh = false) {
   $request = "/wishlist?sortfield=t.rang&sortorder=ASC&thirdparty_ids=".$thirdpartyid."&sqlfilters=(t.priv%3A%3D%3A0)";
   $wishlist = callDoliApi("GET", $request, null, dolidelay('product', $refresh));
-  $wish = '<button class="btn btn-sm btn-light" name="wish" value="wish" type="submit" onclick="doliJavaCartAction(\'updateLine\', '.$productid.', 1, \'wish\');"><i class="fas fa-heart" style="color:Fuchsia"></i></button>';
-
+  $wish = '<button class="btn btn-sm btn-light" name="wish" value="wish" type="submit" onclick="doliJavaCartAction(\'updateLine\', '.$productid.', 1, \'wish\');"><i class="fa-solid fa-heart" style="color:Fuchsia"></i></button>';
+  foreach ($wishlist as $wsh) {
+    if ($productid == $wsh->fk_product) {
+      $wish = '<button class="btn btn-sm btn-light" name="wish" value="unwish" type="submit" onclick="doliJavaCartAction(\'updateLine\', '.$productid.', 1, \'wish\');"><i class="fa-solid fa-heart-crack"></i></button>';
+    }
+  }
   return $wish;
 }
 
