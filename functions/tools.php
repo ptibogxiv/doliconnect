80a2190a013a4b4b860e08e1f5d3ef13739383b7
ptibogxiv <support@ptibogxiv.net>
2024-10-05 13:39:14 +0200

    Update tools.php

--- functions/tools.php
+++ functions/tools.php
@@ -1,9 +1,36 @@
 <?php
 
-function dolimenu($name, $traduction, $right, $content) {
+/**
+ * Creates a nicely formatted and more specific title element text
+ * for output in head of document, based on current view.
+ *
+ * @param string $title Default title text for current view.
+ * @param string $sep   Optional separator.
+ * @return string Filtered title.
+ */
+/*
+function doliFilterTitle( $title, $sep ) {
+	global $paged, $page;
 
+	if ( is_feed() )
+		return $title;
 
+	// Add the site name.
+	$title .= get_bloginfo( 'name' );
+
+	// Add the site description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		$title = "$title $sep $site_description";
+
+	// Add a page number if necessary.
+	if ( $paged >= 2 || $page >= 2 )
+		$title = "$title $sep " . sprintf( __( 'Pageggrgregregergre %s', 'twentytwelve' ), max( $paged, $page ) );
+
+	//return 'test test';
 }
+add_filter( 'wp_title', 'doliFilterTitle', 10, 2 );
+*/
 
 function dolicheckie($server) {
   $return = false;
