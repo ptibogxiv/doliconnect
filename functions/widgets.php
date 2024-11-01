7dece2c0fb6eae51b1e1cc49a9e35e2e169c265a
ptibogxiv <support@ptibogxiv.net>
2024-11-01 15:32:37 +0100

    Update widgets.php

--- functions/widgets.php
+++ functions/widgets.php
@@ -308,7 +308,7 @@ print '<a href="'.doliconnecturl('doliaccount').'" title="'.__('My account', 'do
 
 if ( doliconnectid('dolicart') > 0 ) { 
 print '<a href="'.doliconnecturl('dolicart').'" title="'.__('Basket', 'doliconnect').'"><span class="fa-layers fa-fw fa-2x">
-<i class="fas fa-shopping-bag"></i><span class="fa-layers-counter fa-lg" id="DoliWidgetCartItems" style="background:Tomato">'.(!empty(doliconnector( null, 'fk_order_nb_item'))?doliconnector( null, 'fk_order_nb_item'):'0').'</span></span></a>';  
+<i class="fas fa-shopping-bag"></i><span class="fa-layers-counter fa-lg" id="DoliWidgetCartItems" style="background:Tomato">'.doliconnect_countitems(doliConnect('order', wp_get_current_user())).'</span></span></a>';  
 } 
 
 print $args['after_widget'];  
