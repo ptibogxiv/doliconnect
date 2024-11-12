148d09914d5bc19ada366b694cbd8300c973241a
ptibogxiv <support@ptibogxiv.net>
2024-11-12 19:42:07 +0100

    Update tools.php

--- functions/tools.php
+++ functions/tools.php
@@ -83,15 +83,10 @@ function doliConnect($fonction, $current_user = null, $boolean = false, $refresh
     $thirdparty = callDoliApi("GET", "/thirdparties/email/".$current_user->user_email, null, dolidelay('doliconnector'));
     if (isset($thirdparty->id)) $object = callDoliApi("GET", "/orders?sortfield=t.rowid&sortorder=DESC&limit=1&thirdparty_ids=".$thirdparty->id."&sqlfilters=(t.fk_statut%3A%3D%3A'0')and(t.module_source%3A%3D%3A'doliconnect')&pagination_data=true", null, dolidelay('doliconnector', $refresh));
     if ( doliversion('20.0.0') && isset($object->data) ) { $object = $object->data; } elseif ( isset($object) ) { $object = $object; } else  { $object = null; }
-    $return = isset($object[0])?$object[0]:null;
     if (isset($object[0])) {
       $return = $object[0];
     } else {
-      $object = (object) 0;
-      $object->id = 0;
-      $object->email = null;
-      $object->typeid = 0;
-      $return = $object;
+      $return= null;
     }
   } else {
     $object = (object) 0;
