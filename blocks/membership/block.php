c02ebd4072d855a2740b707bb30db117c00c2a05
ptibogxiv <support@ptibogxiv.net>
2025-05-19 19:04:29 +0200

    Update block.php

--- blocks/membership/block.php
+++ blocks/membership/block.php
@@ -62,7 +62,7 @@ if ( !isset($typeadhesion->error) ) {
 	$html .= '<div class="card-body"><small>'.__( 'Note: the admins reserve the right to change your membership in relation to your personal situation. A validation of the membership may be necessary depending on the cases.', 'doliconnect').'</small></div>';
 }
 
-$html .= doliCardFooter($typeadhesion, 'member');
+$html .= doliCardFooter($typeadhesion , 'member', $request);
 $html .= "</div>";
 return $html;
 }
