6a3c8f3fd2dd8eb9d632c4fe5ad3fa500fa025cd
ptibogxiv <support@ptibogxiv.net>
2025-05-31 22:38:18 +0200

    Update block.js

--- blocks/agenda/block.js
+++ blocks/agenda/block.js
@@ -1,18 +1,34 @@
 ( function( editor, components, i18n, element ) {
 const { registerBlockType } = wp.blocks;
 const { createElement } = wp.element;
+const { TextControl } = wp.components;
 
 registerBlockType('doliconnect/agenda-block', {
     title: 'My Custom Block',
     icon: 'smiley',
     category: 'widgets',
-    edit: () => createElement('p', null, 'Hello from the editor!'),
-    save: () => createElement('p', null, 'Hello from the frontend!'),
+    attributes: {
+        content: {
+            type: 'string',
+            default: ''
+        }
+    },
+    edit: (props) => {
+        return createElement(
+            TextControl,
+            {
+                label: 'Texte à afficher',
+                value: props.attributes.content,
+                onChange: (value) => props.setAttributes({ content: value })
+            }
+        );
+    },
+    save: (props) => createElement('p', null, props.attributes.content),
 });
 
 } )(
-	window.wp.editor,
-	window.wp.components,
-	window.wp.i18n,
-	window.wp.element,
+    window.wp.editor,
+    window.wp.components,
+    window.wp.i18n,
+    window.wp.element,
 );
