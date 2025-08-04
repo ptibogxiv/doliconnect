c97d5812d15092e8e625e6de8c0880a9256cca66
ptibogxiv <support@ptibogxiv.net>
2025-08-04 10:26:07 +0200

    fix

--- functions/product.php
+++ functions/product.php
@@ -958,7 +958,7 @@ global $current_user;
     }
 
     if ( doliversion('22.0.0') ) {
-      $request= "/documents?modulepart=product&id=".$product->id."&limit=100&content_type=application%2Fpdfhh&pagination_data=true";
+      $request= "/documents?modulepart=product&id=".$product->id."&limit=100&content_type=application/pdf&pagination_data=true";
       $downloads = callDoliApi("GET", $request, null, dolidelay('document', esc_attr(isset($_GET["refresh"]) ? $_GET["refresh"] : null)));   
       if ( doliversion('22.0.0') && isset($downloads->data) ) { $downloads = $downloads->data; } else { $downloads = $downloads; }
       
