1162b01afad38d733a558b48bffb336fdf22e188
ptibogxiv <support@ptibogxiv.net>
2025-08-06 13:34:42 +0200

    Update product.php

--- functions/product.php
+++ functions/product.php
@@ -555,7 +555,7 @@ global $current_user;
         $button .= doliWishlist($thirdparty, $product->id, $mstock['lineid'], $refresh);
       } 
         $button .= '</div>';
-      $button .= '<div class="form-text" id="basic-addon4"><button type="button" class="btn btn-sm btn-link p-0">Supprimer cet article</button></div>';
+        //$button .= '<div class="form-text" id="basic-addon4"><button type="button" class="btn btn-sm btn-link p-0">Supprimer cet article</button></div>';
 
       if (isset($mstock['step']) && $mstock['step']>1) $button .= '<div class="form-text" id="basic-addon4"><small>'.sprintf(__( 'Sold by %s', 'doliconnect'), $mstock['step']).'</small></div>';  
       $button .= '</div>';
@@ -812,13 +812,14 @@ global $current_user;
 
 $wish = 0;
 $required = 0;
+$list = null;
 if (isset($product->required)) $required = $product->required;
 if (isset($product->fk_product) && !empty($product->qty)) {
 $wish = $product->qty;
 $product = callDoliApi("GET", "/products/".$product->fk_product."?includesubproducts=true&includetrans=true", null, dolidelay('product', esc_attr(isset($_GET["refresh"]) ? $_GET["refresh"] : null)));
-} else {
+} elseif (isset($product->id)) {
 $product = callDoliApi("GET", "/products/".$product->id."?includesubproducts=true&includetrans=true", null, dolidelay('product', esc_attr(isset($_GET["refresh"]) ? $_GET["refresh"] : null)));
-}
+
 $product->required = $required;
 
 $arr_params = array( 'search' => isset($_GET['search'])?$_GET['search']:null, 'category' => isset($_GET['category'])?$_GET['category']:null, 'subcategory' => isset($_GET['subcategory'])?$_GET['subcategory']:null, 'product' => $product->id);  
@@ -862,6 +863,7 @@ $list .= doliProductCart($product, $price, null, esc_attr(isset($_GET["refresh"]
 $list .= "</center></div>";
 }
 $list .= "</div></td></tr></table></li>";
+}
 return $list;
 }
 add_filter( 'doliproductlist', 'doliproductlist', 10, 3);
