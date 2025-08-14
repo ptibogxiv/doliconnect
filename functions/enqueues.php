6aef0f3628431553d5602c48412767b58ce2895b
ptibogxiv <support@ptibogxiv.net>
2025-08-14 19:28:12 +0200

    Update enqueues.php

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -31,7 +31,7 @@ function doliconnect_enqueues() {
   	wp_enqueue_script( 'bootstrap.bundle.min');
 	wp_enqueue_script( 'jquery-masonry');
 	if (empty(get_option('doliconnectfontawesome'))) {
-  		wp_register_script( 'font-awesome', '//use.fontawesome.com/releases/v6.7.2/js/all.js', array(), '6.7.2' );
+  		wp_register_script( 'font-awesome', '//use.fontawesome.com/releases/v7.0.0/js/all.js', array(), '7.0.0' );
 		wp_enqueue_script( 'font-awesome');
 	}
 	//wp_register_script( 'doliconnect-dolicart', plugins_url( 'doliconnect/includes/js/dolicart.js'), array( 'jquery' ), '', false );
