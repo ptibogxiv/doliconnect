58ce3ac19bf1714755032a712e78570eb41e6197
ptibogxiv <support@ptibogxiv.net>
2025-08-15 14:17:45 +0200

    Update product.php

--- functions/product.php
+++ functions/product.php
@@ -593,12 +593,14 @@ function doliOffcanvasCart($current_user) {
   $offcanvas .= doliline(doliConnect('order', $current_user), esc_attr(isset($_GET["refresh"]) ? $_GET["refresh"] : null), false);
   $offcanvas .= '</div>';
   if (doliConnect('order', $current_user)->id > 0 && isset(doliConnect('order', $current_user)->lines) && !empty(doliConnect('order', $current_user)->lines)) {
-    $offcanvas .= '<div class="offcanvas-footer m-3"><div class="d-grid gap-2">';
+    $offcanvas .= '<div class="offcanvas-footer m-3">';
+    $offcanvas .= '<div class="d-grid gap-2">';
     $offcanvas .= "<a type='button' class='btn btn-outline-secondary' href='#' type='submit' onclick='doliJavaCartAction(\"updateCart\", 0, 0, 0, null, \"delete\");'>".__('Empty the basket', 'doliconnect').'</a>';
       $arr_params = array( 'checkout' => wp_create_nonce( 'dolicart-'. doliConnect('order', $current_user)->id.'-'.$current_user->id));  
       $return = esc_url( add_query_arg( $arr_params, doliconnecturl('dolicart')) );
     $offcanvas .= '<a type="button" class="btn btn-primary" href="'.$return.'">'.__('Finaliser la commande', 'doliconnect').'</a>';
-    $offcanvas .= '</div></div>';
+    $offcanvas .= '</div>';
+    $offcanvas .= '</div>';
   }
   return $offcanvas;
 }
