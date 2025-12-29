39c12fdd2211eeb8f87a24cc78649b9ecb5853dc
ptibogxiv <support@ptibogxiv.net>
2025-12-29 19:23:02 +0100

    Update widgets.php

--- functions/widgets.php
+++ functions/widgets.php
@@ -296,7 +296,7 @@ class Doliconnect_DoliShop extends WP_Widget {
 	 */
 public function widget( $args, $instance ) {
 
-if ( !empty($instance['display']) || (empty($instance['display']) && is_page(doliconnectid('dolishop')) && !empty(doliconnectid('dolishop'))) ) { 
+if ( !empty($instance['display']) || (is_singular('doliproduct')) || (empty($instance['display']) && is_page(doliconnectid('dolishop')) && !empty(doliconnectid('dolishop'))) ) { 
   
 print $args['before_widget'];
 if ( ! empty( $instance['title'] ) ) {
