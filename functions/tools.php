94a3b90df11181275994a14bb2e8f146057102cc
ptibogxiv <support@ptibogxiv.net>
2025-12-30 19:47:46 +0100

    Update tools.php

--- functions/tools.php
+++ functions/tools.php
@@ -144,6 +144,52 @@ function doliCheckModules($module, $refresh = false) {
   return $return;
 }
 
+  // Ajout d'un champ personnalisé au type de post doliproduct
+  function doliLockPost_meta_box() {
+      add_meta_box(
+          'doliLock_meta_box_callback',
+          __('Restricted access', 'doliconnect'),
+          'doliLock_meta_box_callback',
+          'post',
+          'side',
+          'default'
+      );
+  }
+  add_action('add_meta_boxes', 'doliLockPost_meta_box');
+    // Ajout d'un champ personnalisé au type de post doliproduct
+  function doliLockPage_meta_box() {
+      add_meta_box(
+          'doliLock_meta_box_callback',
+          __('Restricted access', 'doliconnect'),
+          'doliLock_meta_box_callback',
+          'page',
+          'side',
+          'default'
+      );
+  }
+  add_action('add_meta_boxes', 'doliLockPage_meta_box');
+
+  function doliLock_meta_box_callback($post) {
+      // Récupérer la valeur actuelle du champ personnalisé
+      $custom_field_value = get_post_meta($post->ID, '_doliproduct_productid', true);
+      echo '<label for="doliproduct_productid">' . __('Enter value:', 'doliconnect') . '</label>';
+      echo '<input type="text" id="doliproduct_productid" name="doliproduct_productid" value="' . esc_attr($custom_field_value) . '" size="25" />';
+  }
+
+  function doliproduct2_save_productid($post_id) {
+      // Vérifier les autorisations et la validité
+      if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+          return;
+      }
+      if (!current_user_can('edit_post', $post_id)) {
+          return;
+      }
+      if (isset($_POST['doliproduct_productid'])) {
+          update_post_meta($post_id, '_doliproduct_productid', sanitize_text_field($_POST['doliproduct_productid']));
+      }
+  }
+  add_action('save_post', 'doliproduct2_save_productid');
+
 function consecutiveDoliIterationSameCharacter($password, $NbRepeat = null) {
 		if (empty($NbRepeat)) {
 			return true;
