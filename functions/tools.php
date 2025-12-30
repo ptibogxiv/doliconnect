33945df48590e6dcf3dd2ebcab8c4cb7ecaee38d
ptibogxiv <support@ptibogxiv.net>
2025-12-30 21:41:37 +0100

    Update tools.php

--- functions/tools.php
+++ functions/tools.php
@@ -191,6 +191,18 @@ function doliCheckModules($module, $refresh = false) {
   }
   add_action('save_post', 'doliLock_meta_box_save');
 
+    function doliLock_display( $content) {
+    global $post;
+    $dropdown_value = get_post_meta($post->ID, '_doliLock_dropdown_field', true);
+      if ( (is_singular( 'post' ) || is_singular( 'page' ) ) && in_the_loop() && is_main_query() ) {
+          $custom_field_value = get_post_meta( $post->ID, '_doliproduct_productid', true );
+          $custom_message = '<div class="doliproduct-message">' . sprintf( __( 'This is a doliproduct post. Item N°: %s', 'doliconnect' ), esc_html( $custom_field_value ) ) . '</div>';
+          return $custom_message;
+      }
+      return $content;
+  }
+  add_filter( 'the_content', 'doliLock_display', 10);
+
 function consecutiveDoliIterationSameCharacter($password, $NbRepeat = null) {
 		if (empty($NbRepeat)) {
 			return true;
