5b3341411a005c0959d6e811b341d20f05bb9592
ptibogxiv <support@ptibogxiv.net>
2025-12-31 12:16:35 +0100

    Update product.php

--- functions/product.php
+++ functions/product.php
@@ -57,11 +57,11 @@ if ( !empty(get_option('doliconnectbeta')) ) {
   add_action( 'init', 'doliconnect_dolibarrproduct_init' );
   // Limiter le filtre à la page admin des custom post_type doliproduct
   add_filter('use_block_editor_for_post', function($use_block_editor, $post) {
-      if ($post && $post->post_type === 'doliproduct') {
-          return false;
-      }
-      return $use_block_editor;
-  }, 10, 2);
+    if ($post && $post->post_type === 'doliproduct') {
+        return false;
+    }
+    return $use_block_editor;
+}, 10, 2);
   add_filter( 'template_include', 'dolibarrproduct_page_template', 99 );
   function dolibarrproduct_page_template( $template ) {
       if ( get_query_var('post_type') == 'doliproduct'  ) {
