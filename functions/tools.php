12ce7c4d2749872ab98c3170d1addfcbf3d252f9
ptibogxiv <support@ptibogxiv.net>
2026-02-21 22:31:21 +0100

    Update tools.php

--- functions/tools.php
+++ functions/tools.php
@@ -774,7 +774,7 @@ function doliContactList($thirdparty, $limit, $page, $refresh) {
             foreach ($listcontact  as $postcontact) { 
                 $content .= doliModalButton('contact', 'editcontact'.$postcontact->id, '<div class="d-flex w-100 justify-content-between">
                     <h5 class="mb-1">'.($postcontact->civility ? $postcontact->civility : $postcontact->civility_code).' '.$postcontact->firstname.' '.$postcontact->lastname.'</h5>
-                    <small class="text-body-secondary">3 days ago</small></div>
+                    <small class="text-body-secondary">'.$postcontact->poste.'</small></div>
                     <p class="mb-1">'.$postcontact->address.', '.$postcontact->zip.' '.$postcontact->town.'</p>
                     <small class="text-body-secondary">'.$postcontact->email.'</small>', 'button', 'list-group-item lh-condensed list-group-item-action list-group-item-light', $postcontact->id);                                                                           
             }
