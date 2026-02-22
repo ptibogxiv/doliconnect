666921416fbd59ee0346651904a713ed92f74f3d
ptibogxiv <support@ptibogxiv.net>
2026-02-22 17:13:06 +0100

    Update dolicontactinfos.js

--- includes/custom/js/dolicontactinfos.js
+++ includes/custom/js/dolicontactinfos.js
@@ -2,34 +2,35 @@
 	'use strict';
 
 	$(document).ready(function() {
-	$("#dolicontactinfos-form").on("submit", function(e) { 
-		e.preventDefault();
-		e.stopPropagation(); 
-		$("#DoliconnectLoadingModal").modal("show");
-		var $form = $(this);
-		var modalId = $form.find('input[name="modalid"]').val();
-		console.log("dolicontactinfos form id: " + modalId);
-		if (modalId) {
-			$("#doliModal" + modalId).modal("hide");
-		}
-		$("#DoliconnectLoadingModal").one("shown.bs.modal", function (e) { 
-			$.post($form.attr("action"), $form.serialize(), function(response) {
-				if (response.success) {
-					if (document.getElementById("dolicontactinfos-alert") && response.data.hasOwnProperty("message")) {
-						document.getElementById("dolicontactinfos-alert").innerHTML = response.data.message;      
+		$("#dolicontactinfos-form").on("submit", function(event) { 
+			event.preventDefault();
+			event.stopPropagation(); 
+			$("#DoliconnectLoadingModal").modal("show");
+			var $form = $(this);
+			var modalId = $form.find('input[name="modalid"]').val();
+			console.log("dolicontactinfos form id: " + modalId);
+			var modalId2 = $form.find('button[name="case"]').val();
+			console.log("dolicontactinfos form id: " + modalId2);
+			if (modalId) {
+				$("#doliModal" + modalId).modal("hide");
+			}
+			$("#DoliconnectLoadingModal").one("shown.bs.modal", function (e) { 
+				$.post($form.attr("action"), $form.serialize(), function(response) {
+					if (response.success) {
+						if (document.getElementById("dolicontactinfos-alert") && response.data.hasOwnProperty("message")) {
+							document.getElementById("dolicontactinfos-alert").innerHTML = response.data.message;      
+						}
+						if (document.getElementById("dolicontact-list") && response.data.hasOwnProperty("list")) {
+							document.getElementById("dolicontact-list").innerHTML = response.data.list;  
+						}
+					} else {
+						if (document.getElementById("dolicontactinfos-alert") && response.data.hasOwnProperty("message")) {
+							document.getElementById("dolicontactinfos-alert").innerHTML = response.data.message;      
+						}
 					}
-					if (document.getElementById("dolicontact-list") && response.data.hasOwnProperty("list")) {
-						document.getElementById("dolicontact-list").innerHTML = response.data.list;  
-					}
-				} else {
-					if (document.getElementById("dolicontactinfos-alert") && response.data.hasOwnProperty("message")) {
-						document.getElementById("dolicontactinfos-alert").innerHTML = response.data.message;      
-					}
-				}
-				$("#DoliconnectLoadingModal").modal("hide");
-			}, "json");  
+					$("#DoliconnectLoadingModal").modal("hide");
+				}, "json");  
+			});
 		});
-	  });
-
 	});
 })( jQuery );
