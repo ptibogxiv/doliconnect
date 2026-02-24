4ff0a2843b8229fcdaec2d2b8597238eec2fe94b
ptibogxiv <support@ptibogxiv.net>
2026-02-24 18:43:56 +0100

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -1510,7 +1510,7 @@ if ( doliCheckModules('commande') && !empty($productadhesion) ) {
 }
 $content .= '<br><div class="d-grid gap-2">';
 if ( ! empty($adherent) && $adherent->statut != '-2' ) {
-$content .= '<div class="btn-group" role="group" aria-label="Update membership">';
+    $content .= '<div class="btn-group" role="group" aria-label="Update membership">';
     if (empty($adherent->statut)) { 
         $title = __( 'Reactivate my subscription', 'doliconnect');
     } else {
@@ -1521,7 +1521,9 @@ $content .= '<div class="btn-group" role="group" aria-label="Update membership">
         $content .= doliModalButton('resiliatemembership', 'resiliatemembership', __( 'Resiliate', 'doliconnect'), 'button', 'btn btn-outline-secondary'); 
     }
     $content .= '</div>';
- } else { 
+} elseif ( empty($thirdparty->address) || empty($thirdparty->zip) || empty($thirdparty->town) || empty($thirdparty->country_id) || empty($current_user->billing_type) || empty($current_user->billing_birth) || empty($current_user->user_firstname) || empty($current_user->user_lastname) || empty($current_user->user_email)) {
+    $content .= "Pour adhérer, tous les champs doivent être renseignés dans vos <a href='".esc_url( get_permalink(get_option('doliaccount')))."?module=informations&return=".$url."' class='alert-link'>".__( 'Personal informations', 'doliconnect')."</a></div><div class='col-sm-6 col-md-7'>";
+} else {
     $content .= '<div class="d-grid gap-2">';
     $content .= doliModalButton('editmembership', 'editmembership', __('Become a member', 'doliconnect'), 'button' , 'btn btn-outline-secondary btn-block');
     $content .= '</div>';
