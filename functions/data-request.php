ae2da60278ae2b495258836855071ad08727b5e6
ptibogxiv <support@ptibogxiv.net>
2026-02-25 18:00:13 +0100

    Update data-request.php

--- functions/data-request.php
+++ functions/data-request.php
@@ -1263,7 +1263,7 @@ global $current_user;
 		$data = "data:application/pdf;base64,".$_POST['value2'];
 		$modal['header'] = __( 'Download', 'doliconnect');
 		$modal['body'] = null;
-		$modal['footer'] = $_POST['value1'];
+		$modal['footer'] = null;
 		$response['js'] = null;
 		$response['modal'] = doliModalTemplate(sanitize_text_field($_POST['id']), $modal['header'], $modal['body'], $modal['footer'], 'modal-lg', null, 'p-0');
 		wp_send_json_success($response);
