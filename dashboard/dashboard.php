0f347ec2727e5c579664738a9f7f45c37fa31f55
ptibogxiv <support@ptibogxiv.net>
2026-03-01 14:28:15 +0100

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -386,7 +386,7 @@ global $current_user;
                     } else {
                         $default = null;
                     }
-                    $content .= doliModalButton('gateway', 'discount', '<div class="d-flex w-100 justify-content-between">
+                    $content .= doliModalButton('gateway', 'stripe', '<div class="d-flex w-100 justify-content-between">
                     <h5 class="mb-1">'.$name.'</h5>
                     <small class="text-body-secondary">'.$default.'</small></div>
                     <p class="mb-1">'.$method->holder.'</p>
