bd568c539e2986b5747749df0f5cd7fe0abf30d6
ptibogxiv <support@ptibogxiv.net>
2026-05-24 14:48:05 +0200

    Update doliconnect.php

--- doliconnect.php
+++ doliconnect.php
@@ -36,7 +36,7 @@ function doliconnect_textdomain() {
  */
 
 define('DOLIBARR_MINIMUM_VERSION', '19.0.0');
-define('DOLIBARR_LEGAL_VERSION', '23.0.2');
+define('DOLIBARR_LEGAL_VERSION', '23.0.3');
 
 // ********************************************************
 
