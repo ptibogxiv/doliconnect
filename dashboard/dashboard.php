25e5b34ddecb75b7c846f41f2c6733feae3716ba
ptibogxiv <support@ptibogxiv.net>
2026-07-31 21:10:57 +0200

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -1961,71 +1961,10 @@ require_once( ABSPATH . 'wp-content/plugins/two-factor/class-two-factor-core.php
 $content .= "</li>";    
 }
 $content .= '</ul>';
+$content .= '<div class="card-footer">';
+$content .= 'notice info';
+ $content .= '</div>';
 $content .= '</div>';
-
-if (current_user_can('administrator') && !empty(get_option('doliconnectbeta')) ) { 
-//$content .=  '<div class="audio-player">
-//    <audio controls>
-//        <source src="'.esc_url( get_option('doliconnect_audio_file') ).'" type="audio/mpeg">
-//        '._e('Votre navigateur ne supporte pas l\'élément audio.', 'doliconnect').'
-//    </audio>
-//</div>';
-/*
-$content .=  '<style>';
-?>
-.blur{
-  -webkit-filter: blur(5px);
-  -moz-filter: blur(5px);
-  -o-filter: blur(5px);
-  -ms-filter: blur(5px);
-  filter: blur(5px);
-}
-<?php
-$content .=  '</style>';
-*/
-
-function generate_license($suffix = null) {
-    // Default tokens contain no "ambiguous" characters: 1,i,0,o
-    if(isset($suffix)){
-        // Fewer segments if appending suffix
-        $num_segments = 3;
-        $segment_chars = 6;
-    }else{
-        $num_segments = 5;
-        $segment_chars = 5;
-    }
-    $tokens = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
-    $license_string = '';
-    // Build Default License String
-    for ($i = 0; $i < $num_segments; $i++) {
-        $segment = '';
-        for ($j = 0; $j < $segment_chars; $j++) {
-            $segment .= $tokens[rand(0, strlen($tokens)-1)];
-        }
-        $license_string .= $segment;
-        if ($i < ($num_segments - 1)) {
-            $license_string .= '-';
-        }
-    }
-    // If provided, convert Suffix
-    if(isset($suffix)){
-        if(is_numeric($suffix)) {   // Userid provided
-            $license_string .= '-'.strtoupper(base_convert($suffix,10,36));
-        }else{
-            $long = sprintf("%u\n", ip2long($suffix),true);
-            if($suffix === long2ip($long) ) {
-                $license_string .= '-'.strtoupper(base_convert($long,10,36));
-            }else{
-                $license_string .= '-'.strtoupper(str_ireplace(' ','-',$suffix));
-            }
-        }
-    }
-    return $license_string;
-}
-
-//$content .= generate_license();
-
-}
 
 return $content;
 }
