9fe615f91b14f2fcba9cd32d7646a7c17dca460a
ptibogxiv <support@ptibogxiv.net>
2026-08-03 13:33:43 +0200

    Update tools.php

--- functions/tools.php
+++ functions/tools.php
@@ -1961,13 +1961,15 @@ global $current_user;
       if ( $line->fk_product > 0 ) {
         $product = callDoliApi("GET", "/products/".$line->fk_product."?includesubproducts=true&includetrans=true", null, dolidelay('product', $refresh));
         $mstock = doliProductStock($product, $refresh, true, $line->array_options);
+        $arr_params = array( 'search' => isset($_GET['search'])?$_GET['search']:null, 'category' => isset($_GET['category'])?$_GET['category']:null, 'subcategory' => isset($_GET['subcategory'])?$_GET['subcategory']:null);
+        $producturl = esc_url( add_query_arg( $arr_params, getDoliProductUrl($product->id)) );
       }
       if ($context == 'dolioffcanvascart') {
         $doliline .= '<li class="list-group-item list-group-item-action list-group-item-light d-flex justify-content-start w-100 align-items-center">';
         // todo replace by doliPicture
-        $doliline .= '<svg class="p-2" aria-label="Placeholder: 75x75" class="bd-placeholder-img rounded me-3" height="75" width="75" preserveAspectRatio="xMidYMid slice" role="img" xmlns="http://www.w3.org/2000/svg"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>';
+        $doliline .= '<a href="'.$producturl.'" class="text-decoration-none"><svg class="p-2" aria-label="Placeholder: 75x75" class="bd-placeholder-img rounded me-3" height="75" width="75" preserveAspectRatio="xMidYMid slice" role="img" xmlns="http://www.w3.org/2000/svg"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg></a>';
         $doliline .= '<div class="w-75 align-self-stretch">';
-        $doliline .= '<div class="align-items-start fw-bold">'.doliproduct($line, 'product_label').'</div>';
+        $doliline .= '<div class="align-items-start fw-bold"><a href="'.$producturl.'" class="text-decoration-none text-reset">'.doliproduct($line, 'product_label').'</a></div>';
         $doliline .= '<div class="text-muted"><br></div>';
         $doliline .= '<div class="align-items-end text-muted">';
         $doliline .= '<span class="float-start">';
