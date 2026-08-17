7d8e0938bf3d6bd785bc5c074a34f8bd8f6dd9c8
ptibogxiv <support@ptibogxiv.net>
2026-08-17 18:03:19 +0200

    Update product.php

--- functions/product.php
+++ functions/product.php
@@ -236,9 +236,8 @@ add_action('doliproduct_category_add_form_fields', 'doliproduct_category_add_cus
   function doliconnect_setup_theme() {
    add_theme_support( 'post-thumbnails' );
    add_image_size( 'doliproduct_thumbnail_square', 80, 80, true );
-}
-
-add_action( 'after_setup_theme', 'doliconnect_setup_theme' );
+  }
+  add_action( 'after_setup_theme', 'doliconnect_setup_theme' );
 
 function getDoliProductPostID($productid, $refresh = false) {
   // Vérifier que l'ID du produit est valide
